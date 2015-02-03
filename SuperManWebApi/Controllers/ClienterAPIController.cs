a13740cd06dc5f5e78d61966f7ac68df5d9c7641
caoheyang <cao.heyang@etaostars.com>
2015-02-03 17:04:18 +0800

    测试。

--- SuperManWebApi/Controllers/ClienterAPIController.cs
+++ SuperManWebApi/Controllers/ClienterAPIController.cs
@@ -43,7 +43,7 @@ namespace SuperManWebApi.Controllers
             else if ((!ClienterLogic.clienterLogic().CheckExistPhone(model.recommendPhone))&&(!BusiLogic.busiLogic().CheckExistPhone(model.phoneNo))) //如果推荐人手机号在B端C端都不存在提示信息
                 return ResultModel<ClientRegisterResultModel>.Conclude(CustomerRegisterStatus.PhoneNumberNotExist);
             var clienter = ClientRegisterInfoModelTranslator.Instance.Translate(model);
-            bool result = false;//ClienterLogic.clienterLogic().Add(clienter);
+            bool result =ClienterLogic.clienterLogic().Add(clienter);
             var resultModel = new ClientRegisterResultModel
             {
                 userId = clienter.Id,
