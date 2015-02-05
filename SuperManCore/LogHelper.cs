91fffc337aa4e5a2d8f454632c7d7ed275681e5f
WangChao <wang.chao@etaostars.com>
2015-02-05 11:09:00 +0800

    使用NLog记录日志

---
+++ SuperManCore/LogHelper.cs
@@ -0,0 +1,301 @@
+﻿using NLog;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Script.Serialization;
+
+namespace SuperManCore
+{
+   public class LogHelper
+    {
+       public static Logger logger = LogManager.GetLogger("SuperManCore.LogHelper");
+
+       /// <summary>
+       /// 无异常日志
+       /// </summary>
+       public static void LogWriter()
+       {
+           try
+           {
+               string logstr = "\r\n------------------start---------------------\r\n";
+               MethodBase m = new StackTrace().GetFrame(1).GetMethod();
+               ParameterInfo[] pm = m.GetParameters();
+               string classname = m.DeclaringType.ToString();
+               string propertyName = m.Name;
+               //写类名
+               logstr = logstr + "函数类名:" + classname + "\r\n";
+               //写函数方法
+               logstr = logstr + "函数名称为:" + propertyName + "\r\n";
+               for (int i = 0; i < pm.Length; i++)
+               {
+                   logstr = logstr + "函数的参数有:" + pm[i].Name.ToString() + "\r\n";
+               }
+               logstr += "------------------end---------------------\r\n";
+               //写日志
+               logger.Info(logstr);
+           }
+           catch (Exception)
+           {
+
+               throw;
+           }
+
+       }
+       /// <summary>
+       /// 无异常备注日志
+       /// </summary>
+       /// <param name="rmark">描述操作</param>
+       public new static void LogWriter(string rmark)
+       {
+           try
+           {
+               string logstr = "\r\n------------------start---------------------\r\n";
+               MethodBase m = new StackTrace().GetFrame(1).GetMethod();
+               ParameterInfo[] pm = m.GetParameters();
+               string classname = m.DeclaringType.ToString();
+               string propertyName = m.Name;
+               logstr = logstr + "备注:" + rmark + "\r\n";
+               //写类名
+               logstr = logstr + "函数类名:" + classname + "\r\n";
+               //写函数方法
+               logstr = logstr + "函数名称为:" + propertyName + "\r\n";
+               for (int i = 0; i < pm.Length; i++)
+               {
+                   logstr = logstr + "函数的参数有:" + pm[i].Name.ToString() + "\r\n";
+               }
+
+               logstr += "------------------end---------------------\r\n";
+               //写日志
+               logger.Info(logstr);
+           }
+           catch (Exception)
+           {
+
+               throw;
+           }
+
+       }
+       /// <summary>
+       /// 日志
+       /// </summary>
+       /// <param name="dec">需要捕获的参数（必须为属性类）</param>
+       public new static void LogWriter(object dec)
+       {
+           try
+           {
+
+
+               string logstr = "\r\n-----------------start----------------------\r\n";
+               MethodBase m = new StackTrace().GetFrame(1).GetMethod();
+               ParameterInfo[] pm = m.GetParameters();
+               string classname = m.DeclaringType.ToString();
+               string propertyName = m.Name;
+               //写类名
+               logstr = logstr + "函数类名:" + classname + "\r\n";
+               //写函数方法
+               logstr = logstr + "函数名称为:" + propertyName + "\r\n";
+               for (int i = 0; i < pm.Length; i++)
+               {
+                   logstr = logstr + "函数的参数有:" + pm[i].Name.ToString() + "\r\n";
+               }
+               JavaScriptSerializer jsonSerializer = new JavaScriptSerializer();
+               logstr = logstr + "函数参数值:" + jsonSerializer.Serialize(dec) + "\r\n";
+               logstr += "------------------end---------------------\r\n";
+               //写日志
+               logger.Info(logstr);
+           }
+           catch (Exception)
+           {
+
+               throw;
+           }
+
+       }
+       /// <summary>
+       /// 备注日志
+       /// </summary>
+       /// <param name="dec">需要捕获的参数（必须为属性类）</param>
+       /// <param name="rmark">描述操作</param>
+       public new static void LogWriter(object dec, string rmark)
+       {
+           try
+           {
+
+
+               string logstr = "\r\n-----------------start----------------------\r\n";
+               MethodBase m = new StackTrace().GetFrame(1).GetMethod();
+               ParameterInfo[] pm = m.GetParameters();
+               string classname = m.DeclaringType.ToString();
+               string propertyName = m.Name;
+               logstr = logstr + "备注:" + rmark + "\r\n";
+               //写类名
+               logstr = logstr + "函数类名:" + classname + "\r\n";
+               //写函数方法
+               logstr = logstr + "函数名称为:" + propertyName + "\r\n";
+               for (int i = 0; i < pm.Length; i++)
+               {
+                   logstr = logstr + "函数的参数有:" + pm[i].Name.ToString() + "\r\n";
+               }
+               JavaScriptSerializer jsonSerializer = new JavaScriptSerializer();
+               logstr = logstr + "函数参数值:" + jsonSerializer.Serialize(dec) + "\r\n";
+               logstr += "------------------end---------------------\r\n";
+               //写日志
+               logger.Info(logstr);
+           }
+           catch (Exception)
+           {
+
+               throw;
+           }
+
+       }
+       /// <summary>
+       /// 处理带异常的日志
+       /// </summary>
+       /// <param name="ex">异常对象（必须为属性类）</param>
+       public new static void LogWriter(Exception ex)
+       {
+           try
+           {
+
+               string logstr = "\r\n-----------------start----------------------\r\n";
+               MethodBase m = new StackTrace().GetFrame(1).GetMethod();
+               ParameterInfo[] pm = m.GetParameters();
+               string classname = m.DeclaringType.ToString();
+               string propertyName = m.Name;
+               //写类名
+               logstr = logstr + "函数类名:" + classname + "\r\n";
+               //写函数方法
+               logstr = logstr + "函数名称为:" + propertyName + "\r\n";
+               for (int i = 0; i < pm.Length; i++)
+               {
+                   logstr = logstr + "函数的参数有:" + pm[i].Name.ToString() + "\r\n";
+               }
+               logstr = logstr + "函数异常:" + ex.ToString() + "\r\n";
+               logstr += "------------------end---------------------\r\n";
+               //写日志
+               logger.Info(logstr);
+           }
+           catch (Exception)
+           {
+
+               throw;
+           }
+
+       }
+       /// <summary>
+       /// 异常捕获日志
+       /// </summary>
+       /// <param name="ex">异常对象（必须为属性类）</param>
+       /// <param name="rmark">操作简要描述</param>
+       public new static void LogWriter(Exception ex, string rmark)
+       {
+           try
+           {
+
+               string logstr = "\r\n-----------------start----------------------\r\n";
+               MethodBase m = new StackTrace().GetFrame(1).GetMethod();
+               ParameterInfo[] pm = m.GetParameters();
+               string classname = m.DeclaringType.ToString();
+               string propertyName = m.Name;
+               logstr = logstr + "备注:" + rmark + "\r\n";
+               //写类名
+               logstr = logstr + "函数类名:" + classname + "\r\n";
+               //写函数方法
+               logstr = logstr + "函数名称为:" + propertyName + "\r\n";
+               for (int i = 0; i < pm.Length; i++)
+               {
+                   logstr = logstr + "函数的参数有:" + pm[i].Name.ToString() + "\r\n";
+               }
+               logstr = logstr + "函数异常:" + ex.ToString() + "\r\n";
+               logstr += "------------------end---------------------\r\n";
+               //写日志
+               logger.Info(logstr);
+           }
+           catch (Exception)
+           {
+
+               throw;
+           }
+
+       }
+       /// <summary>
+       /// 带参数 异常的日志
+       /// </summary>
+       /// <param name="dec">异常参数对象（必须为属性类）</param>
+       /// <param name="ex">日志异常对象</param>
+       public new static void LogWriter(object dec, Exception ex)
+       {
+           try
+           {
+               string logstr = "\r\n-----------------start----------------------\r\n";
+               MethodBase m = new StackTrace().GetFrame(1).GetMethod();
+               ParameterInfo[] pm = m.GetParameters();
+               string classname = m.DeclaringType.ToString();
+               string propertyName = m.Name;
+               //写类名
+               logstr = logstr + "函数类名:" + classname + "\r\n";
+               //写函数方法
+               logstr = logstr + "函数名称为:" + propertyName + "\r\n";
+               for (int i = 0; i < pm.Length; i++)
+               {
+                   logstr = logstr + "函数的参数有:" + pm[i].Name.ToString() + "\r\n";
+               }
+               JavaScriptSerializer jsonSerializer = new JavaScriptSerializer();
+               logstr = logstr + "函数参数值:" + jsonSerializer.Serialize(dec) + "\r\n";
+               logstr = logstr + "函数异常:" + ex.ToString() + "\r\n";
+               logstr += "------------------end---------------------\r\n";
+               //写日志
+               logger.Info(logstr);
+           }
+           catch (Exception)
+           {
+
+               throw;
+           }
+
+       }
+       /// <summary>
+       /// 带参数 描述 异常日志捕获
+       /// </summary>
+       /// <param name="dec">所要捕获的参数（必须为属性类）</param>
+       /// <param name="ex">异常对象</param>
+       /// <param name="rmark">描述</param>
+       public new static void LogWriter(object dec, Exception ex, string rmark)
+       {
+           try
+           {
+               string logstr = "\r\n-----------------start----------------------\r\n";
+               MethodBase m = new StackTrace().GetFrame(1).GetMethod();
+               ParameterInfo[] pm = m.GetParameters();
+               string classname = m.DeclaringType.ToString();
+               string propertyName = m.Name;
+               //写备注
+               logstr = logstr + "备注:" + rmark + "\r\n";
+               //写类名
+               logstr = logstr + "函数类名:" + classname + "\r\n";
+               //写函数方法
+               logstr = logstr + "函数名称为:" + propertyName + "\r\n";
+               for (int i = 0; i < pm.Length; i++)
+               {
+                   logstr = logstr + "函数的参数有:" + pm[i].Name.ToString() + "\r\n";
+               }
+               JavaScriptSerializer jsonSerializer = new JavaScriptSerializer();
+               logstr = logstr + "函数参数值:" + jsonSerializer.Serialize(dec) + "\r\n";
+               logstr = logstr + "函数异常:" + ex.ToString() + "\r\n";
+               logstr += "------------------end---------------------\r\n";
+               //写日志
+               logger.Info(logstr);
+           }
+           catch (Exception)
+           {
+
+               throw;
+           }
+       }
+    }
+} 
