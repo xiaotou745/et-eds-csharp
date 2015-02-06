869a2c5a183a3abb61f1c0e45a001926c60e25e9
caoheyang <cao.heyang@etaostars.com>
2015-02-06 15:32:56 +0800

    错误记录 代码规范。

--- SuperMan/Global.asax.cs
+++ SuperMan/Global.asax.cs
@@ -19,31 +19,7 @@ namespace SuperMan
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
-
-        /// <summary>
-        /// 注册全局过滤器 add by caoheyang 20150205
-        /// </summary>
-        /// <param name="filters"></param>
-        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
-        {
-            filters.Add(new WebHandleErrorAttribute(), 1);
-            filters.Add(new HandleErrorAttribute(), 2);
-        }
     }
 
 
-    /// <summary>
-    /// 自定义异常处理类  add by caoheyang 20150205
-    /// </summary>
-    public class WebHandleErrorAttribute : HandleErrorAttribute
-    {
-        /// <summary>
-        /// 重写异常处理方法 add by caoheyang 20150205
-        /// </summary>
-        /// <param name="filterContext">上下文对象  该类继承于ControllerContext</param>
-        public override void OnException(ExceptionContext filterContext)
-        {
-            LogHelper.LogWriterFromFilter(filterContext.Exception);
-        }
-    }
 }
--- SuperManWebApi/Global.asax.cs
+++ SuperManWebApi/Global.asax.cs
@@ -20,29 +20,8 @@ namespace SuperManWebApi
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
-        /// <summary>
-        /// 注册全局过滤器 add by caoheyang 20150205
-        /// </summary>
-        /// <param name="filters"></param>
-        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
-        {
-            filters.Add(new ApiHandleErrorAttribute(), 1);
-            filters.Add(new HandleErrorAttribute(), 2);
-        }
+      
     }
 
-    /// <summary>
-    /// 自定义异常处理类  add by caoheyang 20150205
-    /// </summary>
-    public class ApiHandleErrorAttribute : HandleErrorAttribute
-    {
-        /// <summary>
-        /// 重写异常处理方法 add by caoheyang 20150205
-        /// </summary>
-        /// <param name="filterContext">上下文对象  该类继承于ControllerContext</param>
-        public override void OnException(ExceptionContext filterContext)
-        {
-            LogHelper.LogWriterFromFilter(filterContext.Exception);
-        }
-    }
+
 }
