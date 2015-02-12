50d94ede80eca848313e36733021cc08cda6c452
caoheyang <cao.heyang@etaostars.com>
2015-02-12 16:45:49 +0800

    修改权限部分设计bug。

--- SuperManBusinessLogic/Authority_Logic/AuthorityLogic.cs
+++ SuperManBusinessLogic/Authority_Logic/AuthorityLogic.cs
@@ -83,6 +83,10 @@ namespace SuperManBusinessLogic.Authority_Logic
         {
 
         }
+        /// <summary>
+        /// 更新后台用户权限信息
+        /// </summary>
+        /// <param name="model"></param>
         public void UpdateAuthority(AuthorityListModel model)
         {
             using (var db = new supermanEntities())
@@ -92,7 +96,7 @@ namespace SuperManBusinessLogic.Authority_Logic
                 if (account != null)
                 {
                     var oldauid = db.accountauthority.Where(p => p.AccountId.Value == account.Id).ToList();
-                    List<int> existAuid = new List<int>();
+                    List<int> existAuid = new List<int>(); 
                     List<accountauthority> toRemove = new List<accountauthority>();
                     foreach (var oldid in oldauid)
                     {
