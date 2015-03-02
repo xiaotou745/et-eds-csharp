7f5545b81b7b2f9ea224410c4f37fce3fc846304
pingyang <ping.yang@etaostars.com>
2015-03-02 14:39:25 +0800

    查看订单-分配超人

--- SuperMan/Controllers/OrderController.cs
+++ SuperMan/Controllers/OrderController.cs
@@ -1,4 +1,5 @@
-﻿using SuperManBusinessLogic.Order_Logic;
+﻿using SuperManBusinessLogic.C_Logic;
+using SuperManBusinessLogic.Order_Logic;
 using SuperManCommonModel.Entities;
 using SuperManCommonModel.Models;
 using System;
@@ -7,6 +8,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using SuperManCore;
+using SuperManCore.Common;
 using SuperManCore.Paging;
 using SuperManDataAccess;
 
@@ -48,6 +50,27 @@ namespace SuperMan.Controllers
         public ActionResult OrderCount()
         {
             return View();
-        }        
+        }   
+        /// <summary>
+        /// 超人抢单--平扬 2015.3.2
+        /// </summary>
+        /// <param name="SuperID">超人id</param>
+        /// <param name="orderNo">订单号</param>
+        /// <returns></returns>
+
+        [HttpPost]
+        public JsonResult RushOrder(int SuperID, string OrderNo)
+        {
+            if (SuperID == 0) //用户id验证
+                return Json(new ResultModel(false, "超人不能为空"), JsonRequestBehavior.AllowGet);
+            if (string.IsNullOrEmpty(OrderNo)) //订单号码非空验证
+                return Json(new ResultModel(false, "订单不能为空"), JsonRequestBehavior.AllowGet);
+            if (ClienterLogic.clienterLogic().GetOrderByNo(OrderNo) == null) //查询订单是否存在
+                return Json(new ResultModel(false, "订单不存在"), JsonRequestBehavior.AllowGet);
+            if (!ClienterLogic.clienterLogic().CheckOrderIsAllowRush(OrderNo))  //查询订单是否被抢
+                return Json(new ResultModel(false, "订单已被抢或者已完成"), JsonRequestBehavior.AllowGet);
+            var bResult = ClienterLogic.clienterLogic().RushOrder(SuperID, OrderNo);
+            return Json(bResult ? new ResultModel(true, "抢单成功") : new ResultModel(false, "抢单失败"), JsonRequestBehavior.AllowGet);
+        }
     }
 }
