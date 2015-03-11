19de2906cbff79c512b76524cf45bcba192de87d
caoheyang <cao.heyang@etaostars.com>
2015-03-11 17:50:20 +0800

    更改 B端获取订单接口   改为 ado形式。

---
+++ Ets.Service.Provider/User/BusinessProvider.cs.cs
@@ -0,0 +1,28 @@
+﻿using Ets.Dao.User;
+using Ets.Service.IProvider;
+using Ets.Service.IProvider.User;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ets.Service.Provider.User
+{
+
+    /// <summary>
+    /// 商户业务逻辑接口实现类  add by caoheyang 20150311
+    /// </summary>
+    public class BusinessProvider : IBusinessProvider
+    {
+        /// <summary>
+        /// app端商户获取订单   add by caoheyang 20150311
+        /// </summary>
+        /// <param name="user"></param>
+        /// <returns></returns>
+        public virtual IList<int> GetOrdersApp()
+        {
+            return null;
+        }
+    }
+}
