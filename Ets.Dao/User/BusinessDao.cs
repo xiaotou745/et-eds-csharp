6ea84607d0a670975a8220e8c1ee41186c3fdf9c
caoheyang <cao.heyang@etaostars.com>
2015-03-11 20:16:54 +0800

    提交。

--- Ets.Dao/User/BusinessDao.cs
+++ Ets.Dao/User/BusinessDao.cs
@@ -17,10 +17,13 @@ namespace Ets.Dao.User
         /// <summary>
         /// 商户获取订单   add by caoheyang 20150311
         /// </summary>
-        /// <param name="user"></param>
-        public virtual void GetOrdersAppToSql()
-        { 
-        
+        /// <param name="paraModel">查询条件实体</param>
+        public virtual void GetOrdersAppToSql(Ets.Model.ParameterModel.Bussiness.BussOrderParaModelApp paraModel)
+        {
+            string whereStr = "1=1 ";  //where查询条件实体类
+            //if (paraModel.userId != 0)
+                
+            
         }
     }
 }
