1b2a8bd4781256478e5c206ffdd4d83220b3a752
pingyang <ping.yang@etaostars.com>
2015-03-12 17:17:27 +0800

    xx

---
+++ ETS.Core/Util/ConfigSettings.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+namespace ETS.Util
+{
+    public sealed class ConfigSettings
+    {
+        public static readonly ConfigSettings Instance = new ConfigSettings();
+
+        private ConfigSettings() { }
+
+        public string FileUploadPath
+        {
+            get
+            {
+                return ConfigurationManager.AppSettings["FileUploadPath"];
+            }
+        }
+
+        public string FileUploadFolderNameTemp
+        {
+            get
+            {
+                return ConfigurationManager.AppSettings["FileUploadFolderNameTemp"];
+            }
+        }
+
+        public string FileUploadFolderNameCustomerIcon
+        {
+            get
+            {
+                return ConfigurationManager.AppSettings["FileUploadFolderNameCustomerIcon"];
+            }
+        }
+
+        public string FileUploadFolderNameAd
+        {
+            get
+            {
+                return ConfigurationManager.AppSettings["FileUploadFolderNameAd"];
+            }
+        }
+
+        public string FileUploadFolderNameArtwork
+        {
+            get
+            {
+                return ConfigurationManager.AppSettings["FileUploadFolderNameArtwork"];
+            }
+        }
+        public string FileUploadFolderNameArtist
+        {
+            get
+            {
+                return ConfigurationManager.AppSettings["FileUploadFolderNameArtist"];
+            }
+        }
+
+        public string FileUploadFolderNameArtworkType
+        {
+            get
+            {
+                return ConfigurationManager.AppSettings["FileUploadFolderNameArtworkType"];
+            }
+        }
+
+        public string FileUploadFolderNameGallery
+        {
+            get
+            {
+                return ConfigurationManager.AppSettings["FileUploadFolderNameGallery"];
+            }
+        }
+
+        public string ServicePhoneNumber
+        {
+            get
+            {
+                return ConfigurationManager.AppSettings["ServicePhoneNumber"];
+            }
+        }
+        /// <summary>
+        /// WebApi地址
+        /// </summary>
+        public string PicHost
+        {
+            get { return ConfigurationManager.AppSettings["WebApiAddress"]; }
+        }
+        /// <summary>
+        /// 获取Email服务器地址
+        /// </summary>
+        public string EmailFromAdress
+        {
+            get { return ConfigurationManager.AppSettings["EmailFromAddress"]; }
+        }
+        /// <summary>
+        /// 获取Email服务器密码
+        /// </summary>
+        public string EmailPwd
+        {
+            get { return ConfigurationManager.AppSettings["EmailFromPwd"]; }
+        }
+        /// <summary>
+        /// 接收人邮件地址
+        /// </summary>
+        public string EmailToAdress
+        {
+            get { return ConfigurationManager.AppSettings["EmailToAddress"]; }
+        }
+        /// <summary>
+        /// 是否发送邮件
+        /// </summary>
+        public string IsSendMail
+        {
+            get { return ConfigurationManager.AppSettings["IsSendMail"]; }
+        }
+
+
+        /// <summary>
+        /// 是否根据集团推送订单
+        /// </summary>
+        public bool IsGroupPush
+        {
+            get { return Convert.ToBoolean(ConfigurationManager.AppSettings["IsGroupPush"]); }
+        }
+
+
+
+        public string WebSetting(string key)
+        {
+            return ConfigurationManager.AppSettings[key];
+        }
+
+        private string[] _keywords;
+        private const string KEYWORD_FILENAME = "keywords.txt";
+        public string[] TextFilterKeyWords
+        {
+            get
+            {
+                if (_keywords == null)
+                {
+                    var filename = System.IO.Path.Combine(HttpContext.Current.Request.PhysicalApplicationPath, KEYWORD_FILENAME);
+                    _keywords = File.ReadAllLines(filename, Encoding.UTF8);
+                }
+                return _keywords;
+            }
+        }
+    }
+}
---
+++ Ets.Model/Common/ResultInfo.cs
@@ -0,0 +1,86 @@
+﻿//----------------------------------------------------------------------------------
+/// <summary> 
+/// 通用数据返回接口
+/// </summary>
+/// <author> 
+/// 平扬
+/// </author>
+/// <history>
+/// 2014.1.20 Created
+/// </history>
+/// <version> 
+/// 1.00
+/// </version>
+//----------------------------------------------------------------------------------
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ets.Model.Common
+{
+    /// <summary>
+    /// 操作结果类
+    ///<typeparam name="T">所携带数据类型</typeparam>
+    /// </summary>
+    public class ResultInfo<T>
+    {
+        private bool result;
+        private string message;
+        private T data;
+
+        /// <summary>
+        /// 操作是否成功
+        /// </summary>
+        public bool Result
+        {
+            get { return result; }
+            set { result = value; }
+        }
+
+        /// <summary>
+        /// 结果说明信息
+        /// </summary>
+        public string Message
+        {
+            get { return message; }
+            set { message = value; }
+        }
+
+        /// <summary>
+        /// 数据
+        /// </summary>
+        public T Data
+        {
+            get { return data; }
+            set { data = value; }
+        }
+    }
+
+    /// <summary>
+    /// 操作结果类
+    /// </summary>
+    public class ResultInfo
+    {
+        private bool result;
+        private string message;
+
+        /// <summary>
+        /// 操作是否成功
+        /// </summary>
+        public bool Result
+        {
+            get { return result; }
+            set { result = value; }
+        }
+
+        /// <summary>
+        /// 结果说明信息
+        /// </summary>
+        public string Message
+        {
+            get { return message; }
+            set { message = value; }
+        }
+    }
+}
