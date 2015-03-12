8d02f5bd09cf2e2bf26d69942419c915fce6e6a5
pingyang <ping.yang@etaostars.com>
2015-03-12 09:54:34 +0800

    新街口

--- Ets.Dao/Clienter/ClienterDao.cs
+++ Ets.Dao/Clienter/ClienterDao.cs
@@ -1,12 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using ETS.Dao;
+using Ets.Model.DataModel.Clienter;
+using Ets.Model.DataModel.Order;
 
 namespace Ets.Dao.Clienter
 {
-    class ClienterDao
+
+    /// <summary>
+    /// 超人数据层
+    /// </summary>
+    public class ClienterDao : DaoBase
     {
+          //using (var dbEntity = new supermanEntities())
+          //  {
+          //      var query = dbEntity.order.AsQueryable();
+          //      if (!string.IsNullOrWhiteSpace(criteria.city))
+          //          query = query.Where(i => i.business.City == criteria.city.Trim());
+          //      if (!string.IsNullOrWhiteSpace(criteria.cityId))
+          //          query = query.Where(i => i.business.CityId == criteria.cityId.Trim());
+          //      query = query.Where(i => i.Status.Value == ConstValues.ORDER_NEW);0
+          //      query = query.OrderByDescending(i => i.PubDate);
+          //      var result = query.ToList();
+          //      return result;
+          //  }
+        public List<order> GetOrdersNoLoginLatest(ClientOrderSearchCriteria criteria)
+        {
+            string sql = "";
+            return null;
+        }
+
     }
 }
