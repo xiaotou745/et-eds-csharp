7e1f51e0b2262582b6efc906af61d31949850042
WangChao <wang.chao@etaostars.com>
2015-03-14 11:33:23 +0800

    记录一下发单日志

--- SuperManWebApi/Controllers/BusinessAPIController.cs
+++ SuperManWebApi/Controllers/BusinessAPIController.cs
@@ -326,11 +326,15 @@ namespace SuperManWebApi.Controllers
             #region 缓存验证
             string cacheKey = model.userId.ToString() + "_" + model.OrderSign;
             var cacheList = ETS.Cacheing.CacheFactory.Instance[cacheKey];
+            LogHelper.LogWriter("订单发布~商户时间戳", new { cacheKey = cacheKey,model=model });
             if (cacheList != null)
             {
+                LogHelper.LogWriter("cacheList是否存在同一的商户时间戳：", new { cacheList = cacheList });
                 return ResultModel<BusiOrderResultModel>.Conclude(PubOrderStatus.OrderHasExist);//当前时间戳内重复提交,订单已存在 
             }
+            LogHelper.LogWriter("如果存在会继续往下执行？cacheList是否存在商户时间戳：", new { cacheList = cacheList,model=model });
             ETS.Cacheing.CacheFactory.Instance.AddObject(cacheKey, "1", DateTime.Now.AddMinutes(10));//添加当前时间戳记录
+            LogHelper.LogWriter("在缓存里添加时间戳：", new { cacheKey = cacheKey, obj = 1, guoqishijian = DateTime.Now.AddMinutes(10),model=model });
             #endregion
 
             if (model.OrderCount <= 0 || model.OrderCount > 15)   //判断录入订单数量是否符合要求
