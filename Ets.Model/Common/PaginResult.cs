c13faa7979f48499696bb01bd0ce487084d5513d
pingyang <ping.yang@etaostars.com>
2015-03-17 14:06:08 +0800

    新分页控件

--- Ets.Model/Common/PaginResult.cs
+++ Ets.Model/Common/PaginResult.cs
@@ -6,6 +6,45 @@ using System.Threading.Tasks;
 
 namespace Ets.Model.Common
 {
+    public class NewPagingResult
+    {
+        public NewPagingResult()
+        {
+
+        }
+        public NewPagingResult(int pageIndex, int pageSize)
+        {
+            this.PageIndex = pageIndex;
+            this.PageSize = pageSize;
+        }
+
+        public int PageIndex { get; set; }
+        public int PageSize { get; set; }
+
+        public int TotalCount { get; set; }
+        public int RecordCount { get; set; }
+
+        public int TotalPages
+        {
+            get
+            {
+                var val = (float)TotalCount / PageSize;
+                var count = (int)Math.Ceiling(val);
+                return count;
+            }
+        }
+
+        public bool HasPreviousPage
+        {
+            get { return (PageIndex > 1); }
+        }
+
+        public bool HasNextPage
+        {
+            get { return (PageIndex < TotalPages); }
+        }
+    }
+
     public class PagingResult
     {
         public PagingResult()
