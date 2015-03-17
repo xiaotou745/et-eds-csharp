800ffca677dd4a87889f31e8259b4afa28d5be87
douhaichao <dou631@163.com>
2015-03-17 19:17:05 +0800

    d

--- Ets.Service.Provider/Order/OrderCommissionProvider.cs
+++ Ets.Service.Provider/Order/OrderCommissionProvider.cs
@@ -24,7 +24,7 @@ namespace Ets.Service.Provider.Order
             decimal distribe = 0;  //默认外送费，网站补贴都为0
             decimal commissionRate = model.CommissionRate == null ? 0 : Convert.ToDecimal(model.CommissionRate); //佣金比例 
             int orderCount = model.OrderCount == null ? 0 : Convert.ToInt32(model.OrderCount); //佣金比例 
-            if (model.DistribSubsidy != null)//如果外送费有数据，按照外送费计算骑士佣金
+            if (model.DistribSubsidy != null && model.DistribSubsidy > 0)//如果外送费有数据，按照外送费计算骑士佣金
                 distribe = Convert.ToDecimal(model.DistribSubsidy);
             else if (model.WebsiteSubsidy != null)//如果外送费没数据，按照网站补贴计算骑士佣金
                 distribe = Convert.ToDecimal(model.WebsiteSubsidy);
