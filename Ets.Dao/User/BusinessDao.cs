2007832ac136895d3b0c01e7c00a3b139f341cf2
wangxudan <wang.xudan@etaostars.com>
2015-03-17 11:35:20 +0800

    ado

--- Ets.Dao/User/BusinessDao.cs
+++ Ets.Dao/User/BusinessDao.cs
@@ -279,7 +279,7 @@ namespace Ets.Dao.User
                 sbSqlWhere.AppendFormat(" AND b.GroupId={0} ", criteria.GroupId);
             }
             string tableList = @" business  b WITH (NOLOCK)   ";
-            string orderByColumn = " b.InsertTime ";
+            string orderByColumn = " b.InsertTime DESC";
             return new PageHelper().GetPages<T>(SuperMan_Read, criteria.PagingRequest.PageIndex+1, sbSqlWhere.ToString(), orderByColumn, columnList, tableList, criteria.PagingRequest.PageSize, true);
         }
         /// <summary>
