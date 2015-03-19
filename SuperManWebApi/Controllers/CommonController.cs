a0fcf54e301197e91701c706a25e6826808df5b0
douhaichao <dou631@163.com>
2015-03-19 14:32:08 +0800

    d

---
+++ SuperManWebApi/Controllers/CommonController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace SuperManWebApi.Controllers
+{
+    public class CommonController : ApiController
+    {
+        // GET: Common
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
