a7ea5ffcfbffb44ab76ce523689a84255af556c3
wangxudan <wang.xudan@etaostars.com>
2015-03-23 13:56:30 +0800

    用户管理ado

---
+++ Ets.Model/DataModel/Authority/authority.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ets.Model.DataModel.Authority
+{
+    public class authority
+    {
+        public authority()
+        {
+            this.accountauthority = new HashSet<accountauthority>();
+        }
+
+        public int Id { get; set; }
+        public int AuthorityType { get; set; }
+        public string Name { get; set; }
+
+        public virtual ICollection<accountauthority> accountauthority { get; set; }
+    }
+}
---
+++ Ets.Model/ParameterModel/Authority/AuthoritySearchCriteria.cs
@@ -0,0 +1,19 @@
+﻿using Ets.Model.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ets.Model.ParameterModel.Authority
+{
+    public class AuthoritySearchCriteria
+    {
+        public NewPagingResult PagingRequest { get; set; }
+        public string UserName { get; set; }
+        /// <summary>
+        /// 集团id
+        /// </summary>
+        public int? GroupId { get; set; }
+    }
+}
