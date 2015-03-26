0af7a93b6b7a541d387be756775a5d966c037fd9
pingyang <ping.yang@etaostars.com>
2015-03-26 11:01:59 +0800

    结算比例问题

--- Ets.Dao/User/BusinessDao.cs
+++ Ets.Dao/User/BusinessDao.cs
@@ -94,7 +94,7 @@ namespace Ets.Dao.User
             IList<BusinessCommissionModel> list = new List<BusinessCommissionModel>();
             try
             {
-                string sql = " select BB.id,BB.Name,T.Amount,T.OrderCount,isnull(BB.BusinessCommission,0) BusinessCommission, CAST(isnull(BB.BusinessCommission,0) * T.Amount*0.01 as decimal(5,2)) as TotalAmount,@t1 as T1,@t2 as T2 " +
+                string sql = " select BB.id,BB.Name,T.Amount,T.OrderCount,isnull(BB.BusinessCommission,0) BusinessCommission, CAST(isnull(BB.BusinessCommission,0) * T.Amount*0.01 as decimal(10,2)) as TotalAmount,@t1 as T1,@t2 as T2 " +
                          " from business BB with(nolock) inner join " +
                          " (select B.id,B.Name,sum(O.Amount) as Amount,sum(ISNULL(O.OrderCount,1)) as OrderCount " +
                          " from dbo.[order] O with(nolock) inner join dbo.business B with(nolock) on O.businessId=B.Id " +
