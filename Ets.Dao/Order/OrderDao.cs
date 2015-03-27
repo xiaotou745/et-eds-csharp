207f4517c934407460c9974cfdd33ea4a4a6c9dd
douhaichao <dou631@163.com>
2015-03-27 18:05:28 +0800

    d

--- Ets.Dao/Order/OrderDao.cs
+++ Ets.Dao/Order/OrderDao.cs
@@ -673,6 +673,7 @@ namespace Ets.Dao.Order
         public HomeCountTitleModel GetHomeCountTitleToAllDataSql()
         {
             string sql = @"SELECT 
+                        (SELECT SUM (AccountBalance) FROM dbo.clienter(NOLOCK)  WHERE AccountBalance>=1000) AS  WithdrawPrice,--提现金额
                         SUM(ISNULL(Amount,0)) AS OrderPrice, --订单金额
                         COUNT(1) AS MisstionCount,--任务量
                         SUM(ISNULL(OrderCount,0)) AS OrderCount,--订单量
