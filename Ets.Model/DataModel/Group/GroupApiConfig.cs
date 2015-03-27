61e448edd587470787ae625b14f2900fb1055fd9
caoheyang <cao.heyang@etaostars.com>
2015-03-27 12:00:50 +0800

    op

---
+++ Ets.Model/DataModel/Group/GroupApiConfig.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ets.Model.DataModel.Group
+{   
+    /// <summary>
+    /// 集团appkey add by caoheyang 20150327
+    /// </summary>
+    public class GroupApiConfig
+    {
+        /// <summary>
+        /// 应用id
+        /// </summary>
+        public int AppId { get; set; }
+        /// <summary>
+        /// AppKey
+        /// </summary>
+        public string AppKey { get; set; }
+        /// <summary>
+        /// 应用密钥
+        /// </summary>
+        public string AppSecret { get; set; }
+        /// <summary>
+        /// api版本
+        /// </summary>
+        public string AppVersion { get; set; }
+        /// <summary>
+        /// 集团id，万达 3
+        /// </summary>
+        public int GroupId { get; set; }
+    }
+}
--- SuperManWebApi/Controllers/BusinessAPIController.cs
+++ SuperManWebApi/Controllers/BusinessAPIController.cs
@@ -637,9 +637,7 @@ namespace SuperManWebApi.Controllers
         public Ets.Model.Common.ResultModel<bool> CancelOrder_B(string userId, string OrderId)
         {
             if (string.IsNullOrWhiteSpace(OrderId))
-            {
                 return Ets.Model.Common.ResultModel<bool>.Conclude(ETS.Enums.CancelOrderStatus.OrderEmpty);
-            }
             //查询该订单是否存在
             int selResult = iOrderProvider.GetOrderByOrderNo(OrderId);
             if (selResult > 0)
@@ -647,13 +645,9 @@ namespace SuperManWebApi.Controllers
                 //存在的情况下  取消订单  3
                 int cacelResult = iOrderProvider.UpdateOrderStatus(OrderId, Ets.Model.Common.ConstValues.ORDER_CANCEL);
                 if (cacelResult > 0)
-                {
                     return Ets.Model.Common.ResultModel<bool>.Conclude(ETS.Enums.CancelOrderStatus.Success, true);
-                }
                 else
-                {
                     return Ets.Model.Common.ResultModel<bool>.Conclude(ETS.Enums.CancelOrderStatus.FailedCancelOrder, true);
-                }
             }
             else
             {
