0e2b52e00ceae35573cd4d793650c657488c572c
pingyang <ping.yang@etaostars.com>
2015-04-07 17:23:42 +0800

    后台增加按时间补贴设置功能

--- SuperMan/Controllers/SubsidyManagerController.cs
+++ SuperMan/Controllers/SubsidyManagerController.cs
@@ -1,13 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
-using SuperManCommonModel.Models;
+using Ets.Model.DomainModel.GlobalConfig;
+using Ets.Service.Provider.Common;
+using SuperMan.App_Start;
 using SuperManCore.Common;
-using SuperManBusinessLogic.Subsidy_Logic;
-using SuperManCommonModel.Entities;
-using SuperManCore.Paging;
 using Ets.Service.IProvider.Subsidy;
 using Ets.Service.Provider.Subsidy;
 
@@ -62,5 +59,91 @@ namespace SuperMan.Controllers
             
             return View();
         }
+        /// <summary>
+        /// 时间补贴设置
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult TimeSubsidies()
+        {
+            var list = new GlobalConfigProvider().GetTimeSubsidies(); 
+            return View(list);
+        }
+        /// <summary>
+        /// 添加时间补贴设置
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost]
+        public JsonResult AddTimeSubsidies(GlobalConfigTimeSubsidies model)
+        {
+            var list = new GlobalConfigProvider().GetTimeSubsidies(); 
+            list.Add(model);
+            var newlist = (from globalConfigTimeSubsidiese in list
+                orderby globalConfigTimeSubsidiese.Time ascending 
+                select globalConfigTimeSubsidiese).ToList();
+            string values = GetTimesValues(newlist);
+            bool b = new GlobalConfigProvider().UpdateTimeSubsidies(UserContext.Current.Name, values, "添加时间补贴设置操作-设置之后的值:" + values);
+            return Json(new ResultModel(b, string.Empty), JsonRequestBehavior.AllowGet);
+        }
+
+        /// <summary>
+        /// 删除时间补贴设置
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public JsonResult DeleteTimeSubsidies(int id)
+        {
+            //读取全部时间配置
+            var list = new GlobalConfigProvider().GetTimeSubsidies(); 
+            //移除某一时间配置
+            var newlist = (from globalConfigTimeSubsidiese in list
+                           where globalConfigTimeSubsidiese.Id!=id
+                           select globalConfigTimeSubsidiese).ToList();
+            string values = GetTimesValues(newlist);
+            bool b = new GlobalConfigProvider().UpdateTimeSubsidies(UserContext.Current.Name, values, "删除时间补贴设置-设置之后的值:" + values);
+            return Json(new ResultModel(b, string.Empty), JsonRequestBehavior.AllowGet);
+        }
+          
+        /// <summary>
+        /// 修改时间补贴设置
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost]
+        public JsonResult UpdateTimeSubsidies(GlobalConfigTimeSubsidies model)
+        {
+            //读取全部时间配置
+            var list = new GlobalConfigProvider().GetTimeSubsidies();
+
+            var mm = list.FirstOrDefault(subsidies => subsidies.Id == model.Id);
+            if (mm != null)
+            {
+                mm.Time = model.Time;
+                mm.Price = model.Price;
+            }
+            var newlist = (from globalConfigTimeSubsidiese in list
+                           orderby globalConfigTimeSubsidiese.Time ascending
+                           select globalConfigTimeSubsidiese).ToList();
+            string values = GetTimesValues(newlist);
+            bool b = new GlobalConfigProvider().UpdateTimeSubsidies(UserContext.Current.Name, values, "修改时间补贴设置-设置之后的值:" + values);
+            return Json(new ResultModel(b, string.Empty), JsonRequestBehavior.AllowGet);
+        }
+
+        /// <summary>
+        /// 获取时间补贴字符串
+        /// </summary>
+        /// <param name="newlist"></param>
+        /// <returns></returns>
+        private string GetTimesValues(IEnumerable<GlobalConfigTimeSubsidies> newlist)
+        {
+            string values = "";
+            int i = 1;
+            foreach (var globalConfigTimeSubsidiese in newlist)
+            {
+                string aa = globalConfigTimeSubsidiese.Time + "," + globalConfigTimeSubsidiese.Price + "," + i;
+                values += aa + ";";
+                i++;
+            }
+            return values;
+        }
+
     }
 }
