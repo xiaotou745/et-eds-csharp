ad1b71b84a8d650bb600d41c79c7bd62d4ac76da
douhaichao <dou631@163.com>
2015-04-16 13:41:14 +0800

    d

--- ETS.Core/Util/JsonHelper.cs
+++ ETS.Core/Util/JsonHelper.cs
@@ -18,5 +18,15 @@ namespace Letao.Util
 
             return serializer.Deserialize<T>(json);
         }
+
+        public static T JsonConvertToObject<T>(string json) where T : class
+        {
+            return Newtonsoft.Json.JsonConvert.DeserializeObject<T>(json);
+        }
+
+        public static string JsonConvertToString(object o)
+        {
+            return Newtonsoft.Json.JsonConvert.SerializeObject(o);
+        }
     }
 }
