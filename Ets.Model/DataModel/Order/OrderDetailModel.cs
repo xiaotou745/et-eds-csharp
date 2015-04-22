20231ea638143f1e6f54e54dcb15c75e9e6956bb
pingyang <ping.yang@etaostars.com>
2015-04-22 17:28:07 +0800

    订单详细实体类

---
+++ Ets.Model/DataModel/Order/OrderDetailModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ets.Model.DataModel.Order
+{
+    public class OrderDetailModel
+    {
+        public string OrderNo { get; set; }
+        public string ProductName { get; set; }
+        public decimal UnitPrice { get; set; }
+        public int Quantity { get; set; }
+    }
+}
