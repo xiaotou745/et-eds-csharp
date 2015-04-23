d8aabb8e03a26359db3be19248c914704dbd0757
xupengcheng <xu.pengcheng@etaostars.com>
2015-04-23 10:39:29 +0800

    merge分支

---
+++ Ets.Service.Provider/OpenApi/HomeForDinnerGroup.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ets.Service.IProvider;
+namespace Ets.Service.Provider.OpenApi
+{
+    /// <summary>
+    /// 回家吃饭对接业务
+    /// 徐鹏程
+    /// 2015-04-23
+    /// </summary>
+    public class HomeForDinnerGroup 
+    {
+
+
+    }
+}
