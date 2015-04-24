5f3b2169318a8dc1fc4a275ce8dc7aadaabdba04
caoheyang <cao.heyang@etaostars.com>
2015-04-24 16:24:04 +0800

    美团专用八分钟无人抢单取消订单回调

--- OpenApi/Controllers/MeiTuanController.cs
+++ OpenApi/Controllers/MeiTuanController.cs
@@ -35,7 +35,6 @@ namespace OpenApi.Controllers
 {
     public class MeiTuanController : ApiController
     {
-        // POST: Order PullOrder   paramodel 固定 必须是 paramodel  
         /// <summary>
         /// 接受美团发布订单推送的订单信息    add by caoheyang 20150420  目前仅支持美团
         /// </summary>
@@ -49,18 +48,50 @@ namespace OpenApi.Controllers
                 //实体类赋值
                 MeiTuanOrdeModel paramodel = HTTPHelper.BindeModel<MeiTuanOrdeModel>(HttpContext.Current.Request);
                 MeiTuanGroup meituan = new MeiTuanGroup();
-                if (meituan.ValiditeSig(HttpContext.Current.Request))
+                if (meituan.GetSig(HttpContext.Current.Request) == paramodel.sig)
                 {
                     CreatePM_OpenApi model = meituan.TranslateModel(paramodel);
                     return meituan.AddOrder(model) > 0 ? new { data = "ok" } : new { data = "fail" };
                 }
                 return new { data = "fail" };  //推送失败
             }
-            catch (Exception ex) {
+            catch (Exception ex)
+            {
                 LogHelper.LogWriterFromFilter(ex);  //记录日志
                 return new { data = "fail" };  //推送失败
             }
-          
         }
+
+        /// <summary>
+        /// 美团更新E代送订单状态   add by caoheyang 20150421  目前美团专用  
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost]
+        [ExecuteTimeApi]
+        public object ChangeStatus()
+        {
+            try
+            {
+                MeiTuanGroup mgroup = new MeiTuanGroup();
+                ChangeStatusPM_OpenApi model = new ChangeStatusPM_OpenApi();
+                model.orderfrom = OrderConst.OrderFrom4;// 订单来源  美团订单的订单来源是 4
+                model.status = OrderConst.OrderStatus3;// 取消订单
+                model.order_no = HttpContext.Current.Request.Form["order_id"];// 订单号
+                if (HttpContext.Current.Request.Form["sig"] == mgroup.GetSig(HttpContext.Current.Request))
+                {
+                    ResultModel<object> res = new OrderProvider().UpdateOrderStatus_Other(model);
+                    return res.Status == 0 ? new { data = "ok" } : new { data = "fail" };
+                }
+                else
+                    return new { data = "fail" };
+            }
+            catch (Exception ex)
+            {
+                LogHelper.LogWriterFromFilter(ex);  //记录日志
+                return new { data = "fail" };  //推送失败
+            }
+        }
+
+
     }
 }
