27abedb3aebafb850f42fe8b9ca8c5096845d225
caoheyang <cao.heyang@etaostars.com>
2015-04-27 15:14:21 +0800

    注释

--- Ets.Service.Provider/OpenApi/MeiTuanGroup.cs
+++ Ets.Service.Provider/OpenApi/MeiTuanGroup.cs
@@ -42,8 +42,6 @@ namespace Ets.Service.Provider.OpenApi
         /// <returns></returns>
         public OrderApiStatusType AsyncStatus(ParaModel<AsyncStatusPM_OpenApi> paramodel)
         {
-            return OrderApiStatusType.Success;
-
             switch (paramodel.fields.status)
             {
                 case OrderConst.OrderStatus1: //已完成
