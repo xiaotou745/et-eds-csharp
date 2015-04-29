bb4967df18e1f697aa3b84f1fe43ad1bac23879c
hulingbo <hu.lingbo.etaostars.com>
2015-04-29 10:25:30 +0800

    跨店奖励统计功能，使用Quarzt.Net添加任务调度

---
+++ Ets.WinService/Ets.CrossShopStatisticLogService/QuartzService.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+using Common.Logging;
+using Quartz.Impl;
+
+namespace Ets.CrossShopStatisticLogService
+{
+    public partial class QuartzService : ServiceBase
+    {
+        private Quartz.IScheduler scheduler;
+
+        public QuartzService()
+        {
+            InitializeComponent();
+            Quartz.ISchedulerFactory schedulerFactory = new StdSchedulerFactory();
+            scheduler = schedulerFactory.GetScheduler();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            scheduler.Start();         
+        }
+
+        protected override void OnStop()
+        {
+            scheduler.Shutdown();          
+        }
+
+        protected override void OnPause()
+        {
+            scheduler.PauseAll();
+        }
+
+        protected override void OnContinue()
+        {
+            scheduler.ResumeAll();
+        }
+
+    }
+
+}
