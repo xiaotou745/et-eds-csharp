a28da35698033dafadab4987eef8f71d08a49227
caoheyang <cao.heyang@etaostars.com>
2015-04-29 13:21:17 +0800

    回滚

---
+++ Ets.WinService/Ets.CrossShopShortMessage/Ets.CrossShopShortMessage/Jobs/Job_ShortMessage.cs
@@ -0,0 +1,33 @@
+﻿using Ets.Dao.GlobalConfig;
+using Ets.Model.DomainModel.GlobalConfig;
+using Ets.Service.Provider.Common;
+using Ets.Service.Provider.Subsidy;
+using ETS;
+using ETS.Util;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Quartz;
+
+namespace Ets.CrossShopShortMessage.Jobs
+{
+    public class Job_ShortMessage : IJob
+    {
+
+        /// <summary>
+        /// 跨店奖励主程序入口
+        /// 徐鹏程
+        /// 20150421
+        /// </summary>
+        public void Execute(IJobExecutionContext context)
+        {
+            SubsidyProvider CrossShop = new SubsidyProvider();
+            ETS.Util.LogHelper.LogWriter(DateTime.Now.ToString() + "短信发送开始");
+            CrossShop.ShortMessage();
+            ETS.Util.LogHelper.LogWriter(DateTime.Now.ToString() + "短信发送完成");
+        }
+    }
+}
