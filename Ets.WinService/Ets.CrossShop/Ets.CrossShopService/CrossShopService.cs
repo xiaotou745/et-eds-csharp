2c69fd7a09436f776f0de25aa68a303819757c6f
douhaichao <dou631@163.com>
2015-05-09 00:36:12 +0800

    修改跨店服务独立商家

--- Ets.WinService/Ets.CrossShop/Ets.CrossShopService/CrossShopService.cs
+++ Ets.WinService/Ets.CrossShop/Ets.CrossShopService/CrossShopService.cs
@@ -22,8 +22,7 @@ namespace Ets.CrossShopService
 
         protected override void OnStart(string[] args)
         {
-            //Thread.Sleep(1000 * 10);
-            //ETS.Util.Log.WriteTextToFile(DateTime.Now.ToString() + "跨店奖励服务开启", Job_CrossShop.GetLogFilePath(), true);
+            Thread.Sleep(1000 * 10);
             ETS.Util.LogHelper.LogWriter("跨店奖励服务开启" + DateTime.Now.ToString() + ":");
             Thread t = new Thread(Job_CrossShop.RunCrossShop);
             t.Start();  
@@ -31,7 +30,6 @@ namespace Ets.CrossShopService
 
         protected override void OnStop()
         {
-            //ETS.Util.Log.WriteTextToFile(跨店奖励服务结束.Now.ToString() + "跨店奖励服务结束", Job_CrossShop.GetLogFilePath(), true);
             ETS.Util.LogHelper.LogWriter("跨店奖励服务结束" + DateTime.Now.ToString() + ":");
         }
     }
