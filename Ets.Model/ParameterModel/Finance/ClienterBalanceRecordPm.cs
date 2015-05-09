d400bca5bae0deefebc4729b3c1f93e91a35c51c
caoheyang <cao.heyang@etaostars.com>
2015-05-09 15:54:35 +0800

    ClienterBalanceRecord 相关

---
+++ Ets.Model/ParameterModel/Finance/ClienterBalanceRecordPm.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ets.Model.ParameterModel.Finance
+{
+    /// <summary>
+    ///  骑士余额流水表 查询实体类 
+    /// add by caoheyang 20150509
+    /// </summary>
+    public class ClienterBalanceRecordPM
+    {
+    }
+}
---
+++ Ets.Service.IProvider/Finance/IClienterBalanceRecordProvider.cs
@@ -0,0 +1,50 @@
+﻿#region
+using Ets.Model.DataModel.Finance;
+using Ets.Model.ParameterModel.Finance;
+using System.Collections.Generic;
+#endregion
+
+namespace Ets.Service.IProvider.Finance
+{
+    /// <summary>
+    ///  骑士余额流水表 业务逻辑类IClienterBalanceRecordProvider 的摘要说明。
+    /// Generate By: tools.etaoshi.com
+    /// Generate Time: 2015-05-09 14:59:36
+    /// </summary>
+    public interface IClienterBalanceRecordProvider
+    {
+        /// <summary>
+        /// 新增一条记录
+        ///<param name="clienterBalanceRecord">要新增的对象</param>
+        /// </summary>
+        long Create(ClienterBalanceRecord clienterBalanceRecord);
+
+        /// <summary>
+        /// 修改一条记录
+        ///<param name="clienterBalanceRecordPm">要修改的对象</param>
+        /// </summary>
+        void Modify(ClienterBalanceRecord clienterBalanceRecordPm);
+
+        /// <summary>
+        /// 删除一条记录
+        /// </summary>
+        /// <param name="id">id</param>
+        void Remove(long id);
+
+        /// <summary>
+        /// 根据Id得到一个对象实体
+        /// </summary>
+        /// <param name="id">id</param>
+        /// <returns></returns>
+        ClienterBalanceRecord GetById(long id);
+
+        /// <summary>
+        /// 查询方法 
+        /// </summary>
+        /// <param name="clienterBalanceRecordPm">参数实体</param>
+        /// <returns></returns>
+        IList<ClienterBalanceRecord> Query(ClienterBalanceRecordPM clienterBalanceRecordPm);
+
+    }
+
+}
