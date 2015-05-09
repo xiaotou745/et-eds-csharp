000470b08ae178b211f97b4203d7f5177861bc84
caoheyang <cao.heyang@etaostars.com>
2015-05-09 18:30:46 +0800

    将文件包括到项目中

---
+++ Ets.Service.Provider/Finance/ClienterFinanceProvider.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ets.Service.IProvider.Finance;
+
+namespace Ets.Service.Provider.Finance
+{
+    public class ClienterFinanceProvider : IClienterFinanceProvider
+    {
+    }
+}
