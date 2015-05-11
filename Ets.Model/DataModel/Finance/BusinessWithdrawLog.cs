ebaf272e998435297f35bdde0ca57dc6b79f1f65
wangxudan <wang.xudan@etaostars.com>
2015-05-11 16:58:25 +0800

    商户提现管理

---
+++ Ets.Model/DataModel/Finance/BusinessWithdrawLog.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ets.Model.DataModel.Finance
+{
+    public class BusinessWithdrawLog
+    {
+        /// <summary>
+        /// 自增ID(PK)
+        /// </summary>
+        public long Id { get; set; }
+        /// <summary>
+        /// 提现单ID
+        /// </summary>
+        public long WithwardId { get; set; }
+        /// <summary>
+        /// 操作后状态
+        /// </summary>
+        public int Status { get; set; }
+        /// <summary>
+        /// 备注
+        /// </summary>
+        public string Remark { get; set; }
+        /// <summary>
+        /// 操作人
+        /// </summary>
+        public string Operator { get; set; }
+        /// <summary>
+        /// 操作时间
+        /// </summary>
+        public DateTime OperatTime { get; set; }
+
+    }
+}
--- Ets.Service.Provider/Finance/BusinessFinanceProvider.cs
+++ Ets.Service.Provider/Finance/BusinessFinanceProvider.cs
@@ -1,4 +1,5 @@
 ﻿using Ets.Dao.Finance;
+using Ets.Model.DataModel.Finance;
 using Ets.Model.DomainModel.Finance;
 using Ets.Model.ParameterModel.Finance;
 using Ets.Service.IProvider.Finance;
@@ -23,6 +24,46 @@ namespace Ets.Service.Provider.Finance
         public PageInfo<BusinessWithdrawFormModel> GetBusinessWithdrawList(BusinessWithdrawSearchCriteria criteria)
         {
             return businessFinanceDao.GetBusinessWithdrawList<BusinessWithdrawFormModel>(criteria);
+        }
+        /// <summary>
+        /// 根据申请单Id获取商家提现申请单
+        /// danny-20150511
+        /// </summary>
+        /// <param name="Id"></param>
+        /// <returns></returns>
+        public BusinessWithdrawFormModel GetBusinessWithdrawListById(string withwardId)
+        {
+            return businessFinanceDao.GetBusinessWithdrawListById(withwardId);
+        }
+        /// <summary>
+        /// 获取商户提款单操作日志
+        /// danny-20150511
+        /// </summary>
+        /// <param name="withwardId"></param>
+        /// <returns></returns>
+        public IList<BusinessWithdrawLog> GetBusinessWithdrawOptionLog(string withwardId)
+        {
+            return businessFinanceDao.GetBusinessWithdrawOptionLog(withwardId);
+        }
+        /// <summary>
+        /// 审核商户提现申请单
+        /// danny-20150511
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
+        public bool BusinessWithdrawAudit(BusinessWithdrawLog model)
+        {
+            return businessFinanceDao.BusinessWithdrawAudit(model);
+        }
+        /// <summary>
+        /// 商户提现申请单确认打款
+        /// danny-20150511
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
+        public bool BusinessWithdrawPayOk(BusinessWithdrawLog model)
+        {
+            return businessFinanceDao.BusinessWithdrawPayOk(model);
         }
     }
 }
