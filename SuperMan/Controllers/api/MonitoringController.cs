bc086fa635c2c615560be4b3321082bcfffcdb5d
wangyuchuan <wang.yuchuan@etaoshi.com>
2015-05-14 17:28:57 +0800

    add some files.

---
+++ SuperMan/Controllers/api/MonitoringController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace SuperMan.Controllers.api
+{
+    /// <summary>
+    /// 监控服务
+    /// </summary>
+    public class MonitoringController : ApiController
+    {
+        [HttpGet]
+        public string Index()
+        {
+            return "success";
+        }
+    }
+}
