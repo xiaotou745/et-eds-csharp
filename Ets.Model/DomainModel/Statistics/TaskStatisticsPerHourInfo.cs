3f434ad6280dda0b6dce4dac5f0ce7e3bdab0bb0
wangyuchuan <wang.yuchuan@etaoshi.com>
2015-05-21 21:53:17 +0800

    添加每小时任务统计和接单完成时长统计

---
+++ Ets.Model/DomainModel/Statistics/TaskStatisticsPerHourInfo.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Ets.Model.DomainModel.Statistics
+{
+    /// <summary>
+    /// 每小时任务数量
+    /// </summary>
+    public class TaskStatisticsPerHourInfo
+    {
+        /// <summary>
+        /// 日期
+        /// </summary>
+        public string Date { get; set; }
+
+        /// <summary>
+        /// 城市
+        /// </summary>
+        public string City { get; set; }
+
+        public IList<int> TaskCounts { get; set; }
+
+        ///// <summary>
+        ///// 0点任务数量
+        ///// </summary>
+        //public int Count0 { get; set; }
+
+        ///// <summary>
+        ///// 1点任务数量
+        ///// </summary>
+        //public int Count1 { get; set; }
+
+        //public int Count2 { get; set; }
+        //public int Count3 { get; set; }
+        //public int Count4 { get; set; }
+        //public int Count5 { get; set; }
+        //public int Count6 { get; set; }
+        //public int Count7 { get; set; }
+        //public int Count8 { get; set; }
+        //public int Count9 { get; set; }
+        //public int Count10 { get; set; }
+        //public int Count11 { get; set; }
+        //public int Count12 { get; set; }
+        //public int Count13 { get; set; }
+        //public int Count14 { get; set; }
+        //public int Count15 { get; set; }
+        //public int Count16 { get; set; }
+        //public int Count17 { get; set; }
+        //public int Count18 { get; set; }
+        //public int Count19 { get; set; }
+        //public int Count20 { get; set; }
+        //public int Count21 { get; set; }
+        //public int Count22 { get; set; }
+        //public int Count23 { get; set; }
+    }
+
+    public class ParamTaskPerHour
+    {
+        /// <summary>
+        /// 开始日期
+        /// </summary>
+        public DateTime? StartDate { get; set; }
+
+        /// <summary>
+        /// 结束日期
+        /// </summary>
+        public DateTime? EndDate { get; set; }
+
+        /// <summary>
+        /// 是否按照城市来查询
+        /// </summary>
+        public bool AsCityQuery { get; set; }
+    }
+}
--- SuperMan/Controllers/OrderStatisticalController.cs
+++ SuperMan/Controllers/OrderStatisticalController.cs
@@ -1,20 +1,21 @@
-﻿using Ets.Service.IProvider.Order;
+﻿using System.Web.Mvc;
+using Ets.Service.IProvider.Order;
 using Ets.Service.Provider.Order;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
 
 namespace SuperMan.Controllers
 {
     public class OrderStatisticalController : Controller
     {
         // GET: OrderCount
-        IOrderProvider iOrderProvider = new OrderProvider();
+        private readonly IOrderProvider iOrderProvider = new OrderProvider();
+
         public ActionResult OrderStatistical()
         {
-            var criteria = new Ets.Model.DomainModel.Subsidy.HomeCountCriteria() { PagingRequest = new Ets.Model.Common.NewPagingResult(1, Ets.Model.Common.ConstValues.Web_PageSize), searchType = 1 };
+            var criteria = new Ets.Model.DomainModel.Subsidy.HomeCountCriteria()
+            {
+                PagingRequest = new Ets.Model.Common.NewPagingResult(1, Ets.Model.Common.ConstValues.Web_PageSize),
+                searchType = 1
+            };
             var pagedList = iOrderProvider.GetOrderCount(criteria);
             return View(pagedList);
         }
