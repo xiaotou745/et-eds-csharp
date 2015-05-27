19b8a933ecd14f574a6ed03d80d4886a40bef48a
wangxudan <wang.xudan@etaostars.com>
2015-05-27 13:58:48 +0800

    权限

--- SuperMan/App_Start/RouteConfig.cs
+++ SuperMan/App_Start/RouteConfig.cs
@@ -12,12 +12,12 @@ namespace SuperMan
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "HomeCount", action = "Index", id = UrlParameter.Optional }
-            );
+                defaults:
+                    new {controller = "HomeCount", action = "Index", id = UrlParameter.Optional}
+                );
         }
     }
 }
