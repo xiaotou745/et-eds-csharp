4bb193b6247f55d8d4976a707b762989aca16fa9
caoheyang <cao.heyang@etaostars.com>
2015-06-08 20:14:58 +0800

    骑士获取绑定商家

---
+++ Ets.Model/DomainModel/Bussiness/BCRelationGetByClienterIdDM.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ets.Model.DomainModel.Bussiness
+{
+    /// <summary>
+    /// 根据骑士id查询骑士绑定商家列表 返回结果 
+    /// </summary>
+    public class BCRelationGetByClienterIdDM
+    {
+        /// <summary>
+        /// 主键Id
+        /// </summary>
+        public int Id { get; set; }
+        /// <summary>
+        /// 商户ID
+        /// </summary>
+        public int BusinessId { get; set; }
+        /// <summary>
+        /// 骑士ID
+        /// </summary>
+        public int ClienterId { get; set; }
+        /// <summary>
+        /// 是否有效(0:否 1:是)
+        /// </summary>
+        public int IsEnable { get; set; }
+        /// <summary>
+        /// 创建人
+        /// </summary>
+        public string CreateBy { get; set; }
+        /// <summary>
+        /// 创建时间
+        /// </summary>
+        public DateTime CreateTime { get; set; }
+        /// <summary>
+        /// 最后一次更新人
+        /// </summary>
+        public string UpdateBy { get; set; }
+        /// <summary>
+        /// 最后一次更改时间
+        /// </summary>
+        public DateTime UpdateTime { get; set; }
+        /// <summary>
+        /// 是否绑定(0:否 1:是)
+        /// </summary>
+        public int IsBind { get; set; }
+    }
+}
---
+++ Ets.Service.Provider/Bussiness/BusinessClienterRelationProvider.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ets.Dao.Bussiness;
+using ETS.Data.PageData;
+using Ets.Model.DomainModel.Bussiness;
+using Ets.Model.ParameterModel.Bussiness;
+using Ets.Service.IProvider.Bussiness;
+
+namespace Ets.Service.Provider.Bussiness
+{
+    /// <summary>
+    /// 商户绑定骑士关系   caoheyang 20150608
+    /// </summary>
+   public class BusinessClienterRelationProvider : IBusinessClienterRelationProvider
+    {
+        private readonly BusinessClienterRelationDao businessClienterRelationDao = new BusinessClienterRelationDao();
+       /// <summary> 
+       /// 根据骑士id查询骑士绑定商家列表   caoheyang 20150608
+       /// </summary>
+       public PageInfo<BCRelationGetByClienterIdDM> GetByClienterId(BCRelationGetByClienterIdPM model)
+       {
+           return businessClienterRelationDao.GetByClienterId(model);
+       }
+    }
+}
