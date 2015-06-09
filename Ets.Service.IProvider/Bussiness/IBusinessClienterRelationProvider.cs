311e3084ffb6afc31cbc7d28e8d347b57a770088
hulingbo <hu.lingbo.etaostars.com>
2015-06-09 10:13:08 +0800

    商家对应骑士

--- Ets.Service.IProvider/Bussiness/IBusinessClienterRelationProvider.cs
+++ Ets.Service.IProvider/Bussiness/IBusinessClienterRelationProvider.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 using ETS.Data.PageData;
 using Ets.Model.DomainModel.Bussiness;
 using Ets.Model.ParameterModel.Bussiness;
+using Ets.Model.DataModel.Bussiness;
+using Ets.Model.ParameterModel.Order;
 
 namespace Ets.Service.IProvider.Bussiness
 {
@@ -18,5 +20,33 @@ namespace Ets.Service.IProvider.Bussiness
         /// 根据骑士id查询骑士绑定商家列表   caoheyang 20150608
         /// </summary>
         PageInfo<BCRelationGetByClienterIdDM> GetByClienterId(BCRelationGetByClienterIdPM model);
+
+        /// <summary>
+        /// 增加一条记录
+        /// </summary>
+        /// <UpdateBy>hulingbo</UpdateBy>
+        /// <UpdateTime>20150609</UpdateTime>
+        /// <param name="model"></param>
+        /// <returns></returns>      
+        int Create(BusinessClienterRelation businessClienterRelation);
+
+        /// <summary>
+        /// 更新一条记录
+        /// </summary>
+        /// <UpdateBy>hulingbo</UpdateBy>
+        /// <UpdateTime>20150609</UpdateTime>
+        /// <param name="model"></param>
+        /// <returns></returns>      
+        void Modify(BusinessClienterRelationPM businessClienterRelation);
+
+        /// <summary>
+        /// 获取商户骑士对应关系     
+        /// </summary>
+        /// <UpdateBy>hulingbo</UpdateBy>
+        /// <UpdateTime>20150609</UpdateTime>  
+        /// <param name="model"></param>
+        /// <returns></returns>   
+        BusinessClienterRelation GetDetails(BusinessClienterRelationPM businessClienterRelation);
+
     }
 }
