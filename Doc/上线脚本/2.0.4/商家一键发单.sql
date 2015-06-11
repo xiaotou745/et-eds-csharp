0e97c1e92133c08439d8f97dad782019b03063ec
caoheyang <cao.heyang@etaostars.com>
2015-06-11 14:27:51 +0800

    冲突

--- Doc/上线脚本/2.0.4/商家一键发单.sql
+++ Doc/上线脚本/2.0.4/商家一键发单.sql
@@ -1,15 +1,22 @@
+--商家绑定、一键发单列
 use superman
-alter table dbo.business add OneKeyPubOrder int not null default 1; 
+alter table dbo.business add IsBind smallint not null default 0; 
+alter table dbo.business add OneKeyPubOrder int not null default 0; 
+alter table dbo.clienter add IsBind smallint not null default 0; 
 go
 
+
+
+
+------------初始化北京、上海一键发单数据，countrow可能会变
 declare @countrow INT
   --受影响行数
 --set @countrow = 5433  --预计受影响行数
-select  @countrow=COUNT(1) from dbo.business where City='北京市' or City='上海市'
+select  @countrow=COUNT(1) from dbo.business where City!='北京市' or City!='上海市'
 
 
 begin transaction
-update dbo.business set OneKeyPubOrder=0 where City='北京市' or City='上海市'
+update dbo.business set OneKeyPubOrder=1 where City!='北京市' or City!='上海市'
 if ( @@error <> 0
      or @@rowcount <> @countrow
    ) 
