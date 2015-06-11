21027989aa4f4564addb3b857f3799910f28c2fc
hulingbo <hu.lingbo.etaostars.com>
2015-06-11 10:51:15 +0800

    上线脚本

---
+++ Doc/上线脚本/2.0.4/公共配置.sql
@@ -0,0 +1,16 @@
+use superman
+INSERT dbo.GlobalConfig
+        ( KeyName ,
+          Value ,
+          LastUpdateTime ,
+          Remark ,
+          GroupId ,
+          StrategyId
+        )
+VALUES  ( 'ExclusiveOrderTime', -- KeyName - nvarchar(100)
+          N'5' , -- Value - nvarchar(500)
+          GETDATE() , -- LastUpdateTime - datetime
+          N'商家专属骑士接单响应时间' , -- Remark - nvarchar(200)
+          0 , -- GroupId - int
+          -1  -- StrategyId - int
+        );
