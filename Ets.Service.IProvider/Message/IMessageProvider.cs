d897df4c01e5887b46409a2c6de736aa809480f5
caoheyang <cao.heyang@etaostars.com>
2015-06-15 17:14:04 +0800

    新建类

---
+++ Ets.Service.IProvider/Message/IMessageProvider.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ets.Model.Common;
+using Ets.Model.ParameterModel.Message;
+
+namespace Ets.Service.IProvider.Message
+{
+    /// <summary>
+    /// 消息模块  add by caoheyang 20150615
+    /// </summary>
+    public interface IMessageProvider
+    {
+        /// <summary>
+        /// 商户阅读接口更新消息状态接口 add by caoheyang 20150615
+        /// </summary>
+        /// <param name="model">参数实体</param>
+        /// <returns></returns>
+        ResultModel<object> ReadB(ReadBPM model);
+
+        /// <summary>
+        /// 骑士阅读接口更新消息状态接口 add by caoheyang 20150615
+        /// </summary>
+        /// <param name="model">参数实体</param>
+        /// <returns></returns>
+        ResultModel<object> ReadC(ReadCPM model);
+
+        /// <summary>
+        /// 商户端获取消息列表接口 add by caoheyang 20150615
+        /// </summary>
+        /// <param name="model">参数实体</param>
+        /// <returns></returns>
+        ResultModel<object> ListB(ListBPM model);
+
+        /// <summary>
+        /// 骑士端获取消息列表接口 add by caoheyang 20150615
+        /// </summary>
+        /// <param name="model">参数实体</param>
+        /// <returns></returns>
+        ResultModel<object> ListC(ListCPM model);
+    }
+}
---
+++ Ets.Service.Provider/Message/MessageProvider.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ets.Model.Common;
+using Ets.Model.ParameterModel.Message;
+using Ets.Service.IProvider.Message;
+
+namespace Ets.Service.Provider.Message
+{
+    /// <summary>
+    /// 消息模块  add by caoheyang 20150615
+    /// </summary>
+    public class MessageProvider : IMessageProvider
+    {
+        /// <summary>
+        /// 商户阅读接口更新消息状态接口 add by caoheyang 20150615
+        /// </summary>
+        /// <param name="model">参数实体</param>
+        /// <returns></returns>
+        public ResultModel<object> ReadB(ReadBPM model)
+        {
+            return null;
+        }
+
+        /// <summary>
+        /// 骑士阅读接口更新消息状态接口 add by caoheyang 20150615
+        /// </summary>
+        /// <param name="model">参数实体</param>
+        /// <returns></returns>
+        public ResultModel<object> ReadC(ReadCPM model)
+        {
+            return null;
+        }
+
+        /// <summary>
+        /// 商户端获取消息列表接口 add by caoheyang 20150615
+        /// </summary>
+        /// <param name="model">参数实体</param>
+        /// <returns></returns>
+        public ResultModel<object> ListB(ListBPM model)
+        {
+            return null;
+        }
+
+        /// <summary>
+        /// 骑士端获取消息列表接口 add by caoheyang 20150615
+        /// </summary>
+        /// <param name="model">参数实体</param>
+        /// <returns></returns>
+        public ResultModel<object> ListC(ListCPM model)
+        {
+            return null;
+        }
+    }
+}
