3a2fc49de1240c8840e92d4e5c63bc0b0c185fee
wangxudan <wang.xudan@etaostars.com>
2015-06-16 09:03:42 +0800

    消息编辑

---
+++ SuperMan/Controllers/MessageManagerController.cs
@@ -0,0 +1,31 @@
+﻿using Ets.Service.IProvider.Common;
+using Ets.Service.Provider.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SuperMan.Controllers
+{
+    public class MessageManagerController : Controller
+    {
+        IAreaProvider iAreaProvider = new AreaProvider();
+        // GET: MessageManager
+        public ActionResult MessageManager()
+        {
+            return View();
+        }
+        /// <summary>
+        /// 消息编辑（添加和修改）
+        /// danny-20150615
+        /// </summary>
+        /// <param name="orderNo"></param>
+        /// <returns></returns>
+        public ActionResult MessageEdit()
+        {
+            ViewBag.openCityList = iAreaProvider.GetOpenCityOfSingleCity(0);
+            return View();
+        }
+    }
+}
