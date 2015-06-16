6b25bf619c7b200a8738d86c58be59ac0384bd22
caoheyang <cao.heyang@etaostars.com>
2015-06-16 14:25:54 +0800

    非空验证字段

--- Ets.Model/ParameterModel/Message/ReadBPM.cs
+++ Ets.Model/ParameterModel/Message/ReadBPM.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,9 +16,11 @@ namespace Ets.Model.ParameterModel.Message
         /// 消息ID
         /// </summary>
         public int MessageId { get; set; }
+     
         /// <summary>
-        /// 版本号1.0
+        /// 版本
         /// </summary>
+        [Required(ErrorMessage = "版本号不能为空")]
         public string Version { get; set; }
 
     }
--- Ets.Model/ParameterModel/Message/ReadCPM.cs
+++ Ets.Model/ParameterModel/Message/ReadCPM.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,6 +19,7 @@ namespace Ets.Model.ParameterModel.Message
         /// <summary>
         /// 版本号1.0
         /// </summary>
+        [Required(ErrorMessage = "版本号不能为空")]
         public string Version { get; set; }
     }
 }
