a63dd0876de251e693ee6795f62e8f8a01d2d446
caoheyang <cao.heyang@etaostars.com>
2015-06-17 11:05:54 +0800

    B  C  读消息

---
+++ Ets.Model/DomainModel/Message/ReadCDM.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ets.Model.DomainModel.Message
+{
+    /// <summary>
+    /// 消息详情 add by caoheyang 20150617
+    /// </summary>
+    public class ReadCDM
+    {
+
+        /// <summary>
+        /// 消息体
+        /// </summary>
+        public string Content { get; set; }
+
+        /// <summary>
+        /// 推送时间
+        /// </summary>
+        public string PubDate { get; set; }
+
+    }
+}
