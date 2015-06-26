6b2f732fa66b74ce73ca5f06e5a450a4df9ca970
zhao.hailong <zhao.hailong@etaostars.com>
2015-06-26 09:26:23 +0800

    批量审核时，添加异常处理

--- SuperMan/Controllers/ClienterWithdrawController.cs
+++ SuperMan/Controllers/ClienterWithdrawController.cs
@@ -109,7 +109,7 @@ namespace SuperMan.Controllers
                 if (hasError)
                 {
                     string[] error = result.Where(p => p.Value == false).Select(p => p.Key).ToArray();
-                    totalMsg = string.Join(",", error) + "审核失败,请重试！";
+                    totalMsg = "以下单号审核失败,请重试！\n" + string.Join("\n", error);
                 }
             }
             return Json(new ResultModel(!hasError, totalMsg), JsonRequestBehavior.DenyGet);
