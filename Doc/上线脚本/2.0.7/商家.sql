a2e3531aa71673e97e0b8f86cbb364958e6661ba
douhaichao <dou631@163.com>
2015-06-30 17:25:56 +0800

    更改上线脚本和完成订单时调用redis但没有使用

--- Doc/上线脚本/2.0.7/商家.sql
+++ Doc/上线脚本/2.0.7/商家.sql
@@ -1,3 +1,5 @@
 alter table  dbo.business
 add IsEmployerTask smallint default 0 NOT NULL
 go
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'是否只显示顾主任务，默认0不允许，1允许' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'business', @level2type=N'COLUMN',@level2name=N'IsEmployerTask'
+GO
--- Ets.Service.Provider/Clienter/ClienterProvider.cs
+++ Ets.Service.Provider/Clienter/ClienterProvider.cs
@@ -604,7 +604,7 @@ namespace Ets.Service.Provider.Clienter
             GlobalConfigModel globalSetting = new GlobalConfigProvider().GlobalConfigMethod(0);
             int limitFinish = ParseHelper.ToInt(globalSetting.CompleteTimeSet, 5);
             //取到任务的接单时间、从缓存中读取完成任务时间限制，判断要用户点击完成时间>接单时间+限制时间  
-            var redis = new ETS.NoSql.RedisCache.RedisCache();
+            //var redis = new ETS.NoSql.RedisCache.RedisCache();
             DateTime yuJiFinish = myOrderInfo.GrabTime.Value.AddMinutes(limitFinish);
             if (DateTime.Compare(DateTime.Now, yuJiFinish) < 0)  //小于0说明用户完成时间 太快
             {
