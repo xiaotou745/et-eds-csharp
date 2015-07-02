d62427f9b982e2e450ff27d2bd1fcb64ffba2297
douhaichao <dou631@163.com>
2015-07-02 20:50:40 +0800

    更新导出数据服务

---
+++ Ets.WinService/Ets.ExportData/Ets.ExportData/ExportDataService.cs
@@ -0,0 +1,49 @@
+﻿using Quartz;
+using Quartz.Impl;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Ets.ExportData
+{
+    public partial class ExportDataService : ServiceBase
+    {
+        private Quartz.IScheduler scheduler;
+        public ExportDataService()
+        {
+            Thread.Sleep(10000);
+            InitializeComponent();
+            Quartz.ISchedulerFactory schedulerFactory = new StdSchedulerFactory();
+            scheduler = schedulerFactory.GetScheduler();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            //System.Threading.Thread.Sleep(10000);
+            scheduler.Start();
+        }
+
+
+        protected override void OnStop()
+        {
+            scheduler.Shutdown();
+        }
+
+        protected override void OnPause()
+        {
+            scheduler.PauseAll();
+        }
+
+        protected override void OnContinue()
+        {
+            scheduler.ResumeAll();
+        }
+    }
+}
