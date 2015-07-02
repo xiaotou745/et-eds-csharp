04dd68e076138959eebc729847e2a29f1950b683
ruhuaxiao <ru.huaxiao@etaostars.com>
2015-07-02 08:37:57 +0800

    获取最新代码

---
+++ Ets.WinService/Ets.AddressAssociate/Ets.AddressAssociate/AdressAssociateBLL.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Common.Logging;
+using ETS.Util;
+
+namespace Ets.AddressAssociate
+{
+    public class AdressAssociateBLL : Quartz.IJob
+    {
+        private ILog logger = LogManager.GetCurrentClassLogger();
+        private static bool threadSafe = true;//线程安全
+        public void Execute(Quartz.IJobExecutionContext context)
+        {
+            if (!threadSafe)
+            {
+                return;
+            }
+            threadSafe = false;
+
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+                LogHelper.LogWriter(ex);
+            }
+            finally
+            {
+                threadSafe = true;
+            }
+            //throw new NotImplementedException();
+        }
+    }
+}
