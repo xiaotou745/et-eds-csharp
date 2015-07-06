ba49d57d5709ff36f8edd97490bf1b52874cd230
wangxudan <wang.xudan@etaostars.com>
2015-07-06 13:26:06 +0800

    绑定配送公司

---
+++ SuperMan/Controllers/DeliveryCompanyController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SuperMan.Controllers
+{
+
+    /// <summary>
+    /// 物流公司相关业务 
+    /// </summary>
+    public class DeliveryCompanyController : BaseController
+    {
+        // GET: DeliveryCompany
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+
+        /// <summary>
+        /// 批量导入骑士页面 add by caoheyang 20150706
+        /// </summary>
+        /// <param name="companyId">公司id</param>
+        /// <returns></returns>
+        public ActionResult BatchImportClienter(int companyId)
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// 批量导入骑士  excel  处理  add by caoheyang 20150706
+        /// </summary>
+        /// <param name="companyId">公司id</param>
+        /// <returns></returns>
+        public ActionResult BatchImportClienterExcel(int companyId)
+        {
+            return View();
+        }
+    }
+}
