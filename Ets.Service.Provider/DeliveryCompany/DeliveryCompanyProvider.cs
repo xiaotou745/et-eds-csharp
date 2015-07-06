a04328edab59e5463a57574d34872e53c51e4577
WangChao <wang.chao@etaostars.com>
2015-07-06 13:39:11 +0800

    增加model dao interface

---
+++ Ets.Service.Provider/DeliveryCompany/DeliveryCompanyProvider.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ets.Service.IProvider.DeliveryCompany;
+
+namespace Ets.Service.Provider.DeliveryCompany
+{
+    public class DeliveryCompanyProvider : IDeliveryCompanyProvider
+    {
+    }
+}
