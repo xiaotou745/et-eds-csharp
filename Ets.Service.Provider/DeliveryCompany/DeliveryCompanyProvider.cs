d3599e05fcd428fe1a609720a1aa52515ab91927
caoheyang <cao.heyang@etaostars.com>
2015-07-07 10:24:34 +0800

    解决冲突

--- Ets.Service.Provider/DeliveryCompany/DeliveryCompanyProvider.cs
+++ Ets.Service.Provider/DeliveryCompany/DeliveryCompanyProvider.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Ets.Dao.DeliveryCompany;
 using Ets.Model.Common;
+using Ets.Model.DataModel.DeliveryCompany;
 using Ets.Model.DomainModel.DeliveryCompany;
 using Ets.Service.IProvider.DeliveryCompany;
 
@@ -14,6 +16,17 @@ namespace Ets.Service.Provider.DeliveryCompany
     /// </summary>
     public class DeliveryCompanyProvider : IDeliveryCompanyProvider
     {
+        readonly DeliveryCompanyDao dao = new DeliveryCompanyDao();
+        /// <summary>
+        /// 获取物流公司列表
+        /// danny-20150706
+        /// </summary>
+        /// <returns></returns>
+        public IList<DeliveryCompanyModel> GetDeliveryCompanyList()
+        {
+            return dao.GetDeliveryCompanyList();
+        }
+
         /// <summary>
         /// 物流公司批量导入骑士  add by caoheyang 20150707
         /// </summary>
