1607c0e1793f26297b5fe61f535ae6a16e7c5f65
douhaichao <dou631@163.com>
2015-07-08 15:33:02 +0800

    m.edaisong.com添加注册页

--- Ets.WinService/Ets.AddressAssociate/Ets.AddressAssociate/AdressAssociateBLL.cs
+++ Ets.WinService/Ets.AddressAssociate/Ets.AddressAssociate/AdressAssociateBLL.cs
@@ -14,6 +14,7 @@ namespace Ets.AddressAssociate
     public class AdressAssociateBLL : Quartz.IJob
     {
         private static bool threadSafe = true;//线程安全
+        private static string filePath = System.AppDomain.CurrentDomain.BaseDirectory + "date.xml";
         public void Execute(Quartz.IJobExecutionContext context)
         {
             if (!threadSafe)
@@ -23,25 +24,44 @@ namespace Ets.AddressAssociate
             threadSafe = false;
             try
             {
+                //DateTime currentDate = ParseHelper.ToDatetime("2015-07-06 17:00:00");
+
+                DateTime currentDate = DateTime.Now;
+                //DateModel modelsave = new DateModel()
+                //{
+                //    date = currentDate
+                //};
+                //List<DateModel> listDate1 = new List<DateModel>()
+                //    {
+                //        new DateModel() {date=currentDate}
+                //    };
+                //XmlHelper.ToXml(filePath, listDate1);
                 LogHelper.LogWriterString("扫表开始:时间:" + DateTime.Now.ToString());
-                XmlDocument xdoc = new XmlDocument();
-                string path = System.AppDomain.CurrentDomain.BaseDirectory + @"\date.xml";
-                xdoc.Load(path);
-                XmlNode dateNode = xdoc.SelectSingleNode("date");
-                DateTime sqldate = DateTime.MinValue;
-                DateTime.TryParse(dateNode.Value, out sqldate);
-                DateTime lastdate = DateTime.Now;
+
+                List<DateModel> dateModel = XmlHelper.ToObject<List<DateModel>>(filePath);
+                //XmlDocument xdoc = new XmlDocument();
+
+                //xdoc.Load(path);
+                //XmlNode dateNode = xdoc.SelectSingleNode("date");
+                //DateTime sqldate = DateTime.MinValue;
+                //DateTime.TryParse(dateNode.Value, out sqldate);
+                //DateTime lastdate = DateTime.Now;
                 // 按照 sqldate 查询数据库
-                var receviceAddressDao=new ReceviceAddressDao();
-                var resultcount = receviceAddressDao.GetAddress(sqldate);
-                if ( resultcount> 0)
+                var receviceAddressDao = new ReceviceAddressDao();
+                var resultcount = receviceAddressDao.GetAddress(ParseHelper.ToDatetime(dateModel[0].date, currentDate));
+                if (resultcount > 0)
                 {
                     //本次查询的ID大于上次查询的ID查询成功
-                    XmlElement dateElement = (XmlElement)dateNode;
-                    dateElement.InnerText = lastdate.ToString();
+                    //XmlElement dateElement = (XmlElement)dateNode;
+                    //dateElement.InnerText = lastdate.ToString();
+                    List<DateModel> listDate = new List<DateModel>()
+                    {
+                        new DateModel() {date=currentDate}
+                    };
+                    XmlHelper.ToXml(filePath, listDate);
                 }
-                xdoc.Save(path);
-                LogHelper.LogWriterString("扫表结束:时间:" + DateTime.Now.ToString()+"本次影响行数:"+resultcount);
+                //xdoc.Save(path);
+                LogHelper.LogWriterString("扫表结束:时间:" + DateTime.Now.ToString() + "本次影响行数:" + resultcount);
             }
             catch (Exception ex)
             {
@@ -53,5 +73,10 @@ namespace Ets.AddressAssociate
             }
         }
 
+    }
+
+    public class DateModel
+    {
+        public DateTime date { get; set; }
     }
 }
