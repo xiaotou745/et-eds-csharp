4444da49d110d4bdd08485f9622e7b24d3cce716
pengyi <peng.yi@etaostarts.com>
2015-07-15 18:00:15 +0800

    修改获得银行城市列表

---
+++ Ets.Model/ParameterModel/Finance/BankProvinceCityPM.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ets.Model.ParameterModel.Finance
+{
+    public class BankProvinceCityPM
+    {
+        /// <summary>
+        /// API版本号
+        /// </summary>
+        [Required(ErrorMessage = "API版本号不能为空")]
+        public string Version { get; set; }
+
+        /// <summary>
+        /// 数据版本号
+        /// </summary>
+        [Required(ErrorMessage = "数据版本号不能为空")]
+        public string DataVersion { get; set; }
+    }
+}
