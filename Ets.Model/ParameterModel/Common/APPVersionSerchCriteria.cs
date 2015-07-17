baa55b4b85a732949314a21676225295ecc65a17
wangxudan <wang.xudan@etaostars.com>
2015-07-17 11:27:17 +0800

    App版本控制代码找回

---
+++ Ets.Model/ParameterModel/Common/APPVersionSerchCriteria.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ets.Model.Common;
+
+namespace Ets.Model.ParameterModel.Common
+{
+    public class AppVerionSearchCriteria : ListParaBase
+    {
+    }
+}
