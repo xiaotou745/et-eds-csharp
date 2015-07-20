89cf1fd33b420d35b3bc3ca2bb976b11db78360f
douhaichao <dou631@163.com>
2015-07-20 11:12:18 +0800

    注释掉测试代码

--- SuperManWebApi/Controllers/HomeController.cs
+++ SuperManWebApi/Controllers/HomeController.cs
@@ -19,10 +19,10 @@ namespace SuperManWebApi.Controllers
         {
             //Push.PushMessage(1, "订单提醒", "有订单被抢了！", "有超人抢了订单！", 1761.ToString(), string.Empty);
 
-            GlobalConfigModel globalSetting = new GlobalConfigProvider().GlobalConfigMethod(0);
-            //取到任务的接单时间、从缓存中读取完成任务时间限制，判断要用户点击完成时间>接单时间+限制时间 
-            int limitFinish = ParseHelper.ToInt(globalSetting.CompleteTimeSet, 0);
-            LogHelper.LogWriter("完成时间:" + limitFinish);
+            //GlobalConfigModel globalSetting = new GlobalConfigProvider().GlobalConfigMethod(0);
+            ////取到任务的接单时间、从缓存中读取完成任务时间限制，判断要用户点击完成时间>接单时间+限制时间 
+            //int limitFinish = ParseHelper.ToInt(globalSetting.CompleteTimeSet, 0);
+            //LogHelper.LogWriter("完成时间:" + limitFinish);
             ViewBag.Title = "Home Page";
             return View();
         }
