dbede02772b12453551a657bbbf6226695ab63ff
douhaichao <dou631@163.com>
2015-07-22 17:02:36 +0800

    支持IOS二维码下载

--- SuperMan/Controllers/DownloadController.cs
+++ SuperMan/Controllers/DownloadController.cs
@@ -20,7 +20,15 @@ namespace SuperMan.Controllers
             else
             {
                 ViewBag.IsWeixin = false;
-                Response.Redirect("/Content/app/eds_B.apk?v=" + ETS.Util.TimeHelper.GetTimeStamp());
+                //如果是苹果产品
+                if (CheckAgent())
+                {
+                    Response.Write("<script>window.location='http://fir.im/lazt';</script>");//这里要把易淘食的下载地址放过来 
+                }
+                else
+                {
+                    Response.Redirect("/Content/app/eds_B.apk?v=" + ETS.Util.TimeHelper.GetTimeStamp());
+                }
                 return null;
             }
             ViewBag.UserAgent = userAgent;
@@ -39,12 +47,48 @@ namespace SuperMan.Controllers
             else
             {
                 ViewBag.IsWeixin = false;
-                Response.Redirect("/Content/app/eds_C.apk?v=" + ETS.Util.TimeHelper.GetTimeStamp());
+                //如果是苹果产品
+                if (CheckAgent())
+                {
+                    Response.Write("<script>window.location='http://fir.im/vjk8';</script>");//这里要把易淘食的下载地址放过来 
+                }
+                else
+                {
+                    Response.Redirect("/Content/app/eds_C.apk?v=" + ETS.Util.TimeHelper.GetTimeStamp());
+                }
                 return null;
             }
             ViewBag.UserAgent = userAgent;
 
             return View();
+        }
+
+        /// <summary>
+        /// 判断是否为iphone和ipod和ipad
+        /// 窦海超
+        /// 2015年7月22日 16:39:29
+        /// </summary>
+        /// <returns></returns>
+        private bool CheckAgent()
+        {
+            string agent = HttpContext.Request.UserAgent;
+            string[] keywords = { "iPhone", "iPod", "iPad" };
+            //排除 Windows 桌面系统            
+            if (!agent.Contains("Windows NT") || (agent.Contains("Windows NT") && agent.Contains("compatible; MSIE 9.0;")))
+            {
+                //排除 苹果桌面系统                
+                if (!agent.Contains("Windows NT") && !agent.Contains("Macintosh"))
+                {
+                    foreach (string item in keywords)
+                    {
+                        if (agent.Contains(item))
+                        {
+                            return true;
+                        }
+                    }
+                }
+            }
+            return false;
         }
 
         public ActionResult ViewPage1()
