a5706f9080c2a3e2533dcc8dcec7f886f61e318a
caoheyang <cao.heyang@etaostars.com>
2015-08-12 16:59:14 +0800

    ImprestRecharge 三层

---
+++ Ets.Dao/Finance/ImprestRechargeDao.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ETS.Dao;
+using ETS.Data.Core;
+using ETS.Extension;
+using Ets.Model.DataModel.Finance;
+using ETS.Util;
+
+namespace Ets.Dao.Finance
+{
+    /// <summary>
+    /// 数据访问类ImprestRechargeDao。
+    /// Generate By: tools.etaoshi.com
+    /// Generate Time: 2015-08-12 16:49:48
+    /// add by caoheyang  20150812
+    /// </summary>
+    public class ImprestRechargeDao : DaoBase
+    {
+        #region IImprestRechargeRepos  Members
+
+		/// <summary>
+		/// 增加一条记录
+		/// </summary>
+		public int Insert(ImprestRecharge imprestRecharge)
+		{
+			const string insertSql = @"
+insert into ImprestRecharge(TotalRecharge,RemainingAmount,TotalPayment)
+values(@TotalRecharge,@RemainingAmount,@TotalPayment)
+
+select @@IDENTITY";
+			IDbParameters dbParameters = DbHelper.CreateDbParameters();
+			dbParameters.AddWithValue("TotalRecharge", imprestRecharge.TotalRecharge);
+			dbParameters.AddWithValue("RemainingAmount", imprestRecharge.RemainingAmount);
+			dbParameters.AddWithValue("TotalPayment", imprestRecharge.TotalPayment);
+		    return ParseHelper.ToInt(DbHelper.ExecuteScalar(SuperMan_Write, insertSql, dbParameters));		
+		}
+
+		/// <summary>
+		/// 更新一条记录
+		/// </summary>
+		public void Update(ImprestRecharge imprestRecharge)
+		{
+			const string updateSql = @"
+update  ImprestRecharge
+set  TotalRecharge=@TotalRecharge,RemainingAmount=@RemainingAmount,TotalPayment=@TotalPayment,CreateTime=@CreateTime
+where  Id=@Id ";
+			IDbParameters dbParameters = DbHelper.CreateDbParameters();
+			dbParameters.AddWithValue("Id", imprestRecharge.Id);
+			dbParameters.AddWithValue("TotalRecharge", imprestRecharge.TotalRecharge);
+			dbParameters.AddWithValue("RemainingAmount", imprestRecharge.RemainingAmount);
+			dbParameters.AddWithValue("TotalPayment", imprestRecharge.TotalPayment);
+			dbParameters.AddWithValue("CreateTime", imprestRecharge.CreateTime);
+			DbHelper.ExecuteNonQuery(SuperMan_Write,  updateSql, dbParameters);
+		}
+
+
+
+		/// <summary>
+		/// 根据ID获取对象
+		/// </summary>
+		public ImprestRecharge GetById(int id)
+		{
+
+			const string getbyidSql = @"
+select  Id,TotalRecharge,RemainingAmount,TotalPayment,CreateTime
+from  ImprestRecharge (nolock)
+where  Id=@Id ";
+     			IDbParameters dbParameters = DbHelper.CreateDbParameters();
+          	dbParameters.AddWithValue("Id", id);
+            DataTable dt = DataTableHelper.GetTable(DbHelper.ExecuteDataset(SuperMan_Read, getbyidSql, dbParameters));
+            if (DataTableHelper.CheckDt(dt))
+            {
+                return DataTableHelper.ConvertDataTableList<ImprestRecharge>(dt)[0];
+            }
+            return null;
+		}
+
+		#endregion
+	}
+}
