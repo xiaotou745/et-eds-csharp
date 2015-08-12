bbb741cd50cc9091520613e825557851bbeb95a0
caoheyang <cao.heyang@etaostars.com>
2015-08-12 16:43:24 +0800

    备用金功能

---
+++ SuperMan/Controllers/ImprestController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SuperMan.Controllers
+{
+    /// <summary>
+    /// 备用金controller 
+    /// </summary>
+    public class ImprestController : Controller
+    {
+        /// <summary>
+        /// 备用金充值列表页  add by caoheyang  20150812
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult ImprestRechargeList()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// 备用金充值列表页  异步加载区域 add by caoheyang  20150812
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult DoImprestRechargeList()
+        {
+            return PartialView();
+        }
+
+        /// <summary>
+        /// 备用金充值功能 add by caoheyang  20150812
+        /// </summary>
+        /// <returns></returns>
+        public JsonResult ImprestRecharge()
+        {
+            return new JsonResult();
+        }
+
+        /// <summary>
+        /// 备用金支出列表页 
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult ImprestPaymentList()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// 备用金支出列表页     异步加载区域
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult DoImprestPaymentList()
+        {
+            return PartialView();
+        }
+
+    }
+}
