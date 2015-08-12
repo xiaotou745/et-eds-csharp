0e77b477fc25fa4b77f87004d0e0ddcdc8fbae97
caoheyang <cao.heyang@etaostars.com>
2015-08-12 17:12:18 +0800

    备用金

---
+++ Ets.Service.IProvider/Finance/IImprestBalanceRecordProvider.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ets.Service.IProvider.Finance
+{
+    /// <summary>
+    /// 备用金操作记录  add by caoheyang 20150812
+    /// </summary>
+    public interface IImprestBalanceRecordProvider
+    {
+    }
+}
---
+++ Ets.Service.Provider/Finance/ImprestBalanceRecordProvider.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ets.Service.IProvider.Finance;
+
+namespace Ets.Service.Provider.Finance
+{ 
+    /// <summary>
+    /// 备用金操作记录  add by caoheyang 20150812
+    /// </summary>
+    public class ImprestBalanceRecordProvider : IImprestBalanceRecordProvider
+    {
+    }
+}
