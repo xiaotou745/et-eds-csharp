25fa544ffa908ee137b2b08ce354f08d9ba217cb
ruhuaxiao <ru.huaxiao@etaostars.com>
2015-08-13 13:37:45 +0800

    验证备用金支出逻辑

--- Ets.Dao/Clienter/ClienterDao.cs
+++ Ets.Dao/Clienter/ClienterDao.cs
@@ -202,7 +202,7 @@ where   c.PhoneNo = @PhoneNo
         /// <returns></returns>
         public ClienterModel GetUserInfoByUserId(int UserId)
         {
-            string sql = "SELECT TrueName,PhoneNo,AccountBalance,AllowWithdrawPrice,Status,IDCard  FROM dbo.clienter(NOLOCK) WHERE Id=" + UserId;
+            string sql = "SELECT ID,TrueName,PhoneNo,AccountBalance,AllowWithdrawPrice,Status,IDCard  FROM dbo.clienter(NOLOCK) WHERE Id=" + UserId;
             DataTable dt = DbHelper.ExecuteDataTable(SuperMan_Write, sql);
             IList<ClienterModel> list = MapRows<ClienterModel>(dt);
             if (list == null || list.Count <= 0)
@@ -1117,7 +1117,7 @@ where  Id=@Id ";
             IDbParameters dbParameters = DbHelper.CreateDbParameters();
             dbParameters.AddWithValue("Id", model.Id);
             dbParameters.AddWithValue("WithdrawPrice", model.Money);
-            DbHelper.ExecuteNonQuery(SuperMan_Write, updateSql, dbParameters);
+            var  num=DbHelper.ExecuteNonQuery(SuperMan_Write, updateSql, dbParameters);
         }
 
         /// <summary>
