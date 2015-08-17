1847eaaab9eea006074e6695d137bddb70e96201
douhaichao <dou631@163.com>
2015-08-17 17:27:59 +0800

    单元测试

---
+++ Ets.Provider.Tests/ClienterTests.cs
@@ -0,0 +1,24 @@
+﻿using Ets.Model.ParameterModel.Order;
+using Ets.Service.Provider.Clienter;
+using Ets.Service.Provider.Order;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ets.Provider.Tests
+{
+    [TestFixture]
+    public class ClienterTests
+    {
+        [Test]
+        public void ClienterTest()
+        {
+            ClienterProvider order = new ClienterProvider();
+          
+            order.GetDetails(6381);
+        }
+    }
+}
