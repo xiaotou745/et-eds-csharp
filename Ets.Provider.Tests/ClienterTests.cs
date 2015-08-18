93a59778eb904bb7d6c18c33d677bc4c31aaf3d2
douhaichao <dou631@163.com>
2015-08-18 18:10:06 +0800

    加密

--- Ets.Provider.Tests/ClienterTests.cs
+++ Ets.Provider.Tests/ClienterTests.cs
@@ -18,16 +18,19 @@ namespace Ets.Provider.Tests
         public void ClienterTest()
         {
             //ClienterProvider order = new ClienterProvider();
-          
+
             //order.GetDetails(6381);
 
             string strdes = "eds易代送";
-            //string s = DES.Encrypt3DES(strdes);
-            string s = "2YtqwPeCw15xxIHhs4Zu+9TjY3UR55Q0ZHkrvneEOnjO59tnaqz2S2HYGJgR186hJPOBI3HF1riH6wBQH7zntRecWq/RRAt8vl7hF+Y5QD8=";
-            //string strdecode= DES.Decrypt3DES(s);
-            Console.WriteLine(DESAPP.DES3Decrypt(s, "f83834a7bc7fb26ae7535c7e"));//651554c5
+            string key = "np!u5chin@adm!n1aaaaaaaa";
+            string ss = DESAPP.DES3Encrypt(strdes, key);
 
-            
+            //string s = DES.Encrypt3DES(strdes);
+            string s = "yDjBQC0opeUZVFp9v90oFO8+UMq1uLfx0BhmF+clDp7cPqK2SCRFLygsY0NT9MhJkZ2PDkkxbo94Hj6z4kE99747R0Bx5pxHgz8JkbaorBs=";
+            string strdecode= DES.Decrypt3DES(s);
+            Console.WriteLine();//651554c5
+            string sss = DESAPP.DES3Decrypt(ss, key);
+
 
         }
 
