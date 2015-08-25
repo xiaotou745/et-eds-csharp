5330c9a1a9a7954820172db32eba34cf584c4fe7
wangxudan <wang.xudan@etaostars.com>
2015-08-25 15:14:19 +0800

    地址联想历史数据修复脚本

--- Doc/上线脚本/2.1.5/地址联想表加用户姓名列.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE dbo.ReceviceAddress ADD UserName NVARCHAR(256) DEFAULT '' NOT NULL
-GO 
-EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'”√ªß–’√˚' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'ReceviceAddress', @level2type=N'COLUMN',@level2name=N'UserName'
-go
