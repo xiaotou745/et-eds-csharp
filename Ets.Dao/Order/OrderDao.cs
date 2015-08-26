44dde2ecef9af4522d9e6272dd4a905202a2ee90
WangChao <wang.chao@etaostars.com>
2015-08-26 20:05:48 +0800

    修改拒绝提示bug

--- Ets.Dao/Order/OrderDao.cs
+++ Ets.Dao/Order/OrderDao.cs
@@ -932,7 +932,7 @@ select @@IDENTITY ";
                                         ,o.FinishAll
                                         ,ISNULL(oo.DeductCommissionType,0) DeductCommissionType
                                         ,ISNULL(oo.IsJoinWithdraw,0) IsJoinWithdraw
-                                        ,ISNULL(oo.IsOrderChecked,1) IsOrderChecked
+                                        ,ISNULL(oo.IsOrderChecked,1) IsOrderChecked,o.MealsSettleMode 
                                     FROM [order] o WITH ( NOLOCK )
                                     LEFT JOIN business b WITH ( NOLOCK ) ON b.Id = o.businessId
                                     LEFT JOIN clienter c WITH (NOLOCK) ON o.clienterId=c.Id
