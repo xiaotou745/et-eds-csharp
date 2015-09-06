8b55a6dfc8ba714f2ca2c8f4468f6b2cd22ac566
wangxudan <wang.xudan@etaostars.com>
2015-09-06 16:40:27 +0800

    解决王超的冲突

---
+++ Ets.Model/DomainModel/Finance/ImprestRechargeExcel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ets.Model.DomainModel.Finance
+{
+    public class ImprestRechargeExcel
+    {
+        [Description("充值日期")]
+        public DateTime ChongZhiDate { get; set; }
+        [Description("充值金额")]
+        public decimal ChongZhiAmount { get; set; }
+        [Description("备用金接收人")]
+        public string JieShouRen { get; set; }
+        [Description("操作人")]
+        public string OptName { get; set; }
+        [Description("备注")]
+        public string Remark { get; set; } 
+    }
+}
