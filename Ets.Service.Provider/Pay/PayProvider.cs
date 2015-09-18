6fdab1fa2cfc7399bcb66e6271b25b01d99253b3
douhaichao <dou631@163.com>
2015-09-18 17:36:07 +0800

    优化微信支付

--- Ets.Service.Provider/Pay/PayProvider.cs
+++ Ets.Service.Provider/Pay/PayProvider.cs
@@ -463,7 +463,7 @@ namespace Ets.Service.Provider.Pay
                         LogHelper.LogWriter(fail);
                         return;
                     }
-                    #region 支付宝锁
+                    #region 微信锁
                     //因为最后更新数据库日志时需要时间，但支付宝第二次更新状态访问过来，解决套圈问题.
                     string key = string.Format(RedissCacheKey.AlipayLock, orderNo);
                     var redis = new ETS.NoSql.RedisCache.RedisCache();
@@ -472,7 +472,7 @@ namespace Ets.Service.Provider.Pay
                     {
                         return;
                     }
-                    redis.Set(key, 1, new TimeSpan(0, 1, 0));
+                    redis.Set(key, 1, new TimeSpan(0, 0, 10));
                     #endregion
 
                     ETS.Library.Pay.BWxPay.WxPayData res = new ETS.Library.Pay.BWxPay.WxPayData();
@@ -480,7 +480,7 @@ namespace Ets.Service.Provider.Pay
                     res.SetValue("return_msg", "订单成功");
 
                     //检查该充值单是否已经更新
-                    if (new BusinessRechargeDao().Check(notify.order_no))
+                    if (new BusinessRechargeDao().Check(notify.transaction_id))
                     {
                         HttpContext.Current.Response.Write(res.ToXml());
                         HttpContext.Current.Response.End();
