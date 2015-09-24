8e62c78468aaa8a9118ae4723a03afab86c6d59e
douhaichao <dou631@163.com>
2015-09-24 10:05:13 +0800

    更新上线脚本

--- Doc/上线脚本/2.1.7/2新增列/添加列BusinessBalanceRecord表.sql
+++ Doc/上线脚本/2.1.7/2新增列/添加列BusinessBalanceRecord表.sql
@@ -18,3 +18,12 @@ add GroupAmount decimal(18, 3) default 0  not null
 go
 EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'集团金额' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'BusinessBalanceRecord', @level2type=N'COLUMN',@level2name=N'GroupAmount'
 GO
+
+
+
+
+alter table business
+add LastLoginTime datetime null
+go
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'最后一次登录时间' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'business', @level2type=N'COLUMN',@level2name=N'LastLoginTime'
+GO
