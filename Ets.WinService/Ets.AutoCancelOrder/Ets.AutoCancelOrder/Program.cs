8400de3b5f3eae8f0b2fda80e40f73fd37d8f606
hulingbo <hu.lingbo.etaostars.com>
2015-11-19 23:27:35 +0800

    服务取消订单

---
+++ Ets.WinService/Ets.AutoCancelOrder/Ets.AutoCancelOrder/Program.cs
@@ -0,0 +1,27 @@
+﻿using Ets.Model.Common;
+using Ets.Service.Provider.MyPush;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ets.AutoCancelOrder
+{
+    static class Program
+    {
+        /// <summary>
+        /// 应用程序的主入口点。
+        /// </summary>
+        static void Main()
+        {
+            ServiceBase[] ServicesToRun;
+            ServicesToRun = new ServiceBase[] 
+            { 
+                new AutoCancelOrderService() 
+            };
+            ServiceBase.Run(ServicesToRun);
+        }
+    }
+}
