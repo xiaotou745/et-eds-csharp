b6cd0464aa55f0ca250d4dbbe352c9cb4330e6e5
caoheyang <cao.heyang@etaostars.com>
2015-12-31 14:44:40 +0800

    退单

---
+++ Ets.Dao/Order/ClienterPushLogDao.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ETS.Dao;
+using ETS.Data.Core;
+using Ets.Model.DataModel.Order;
+using ETS.Util;
+
+namespace Ets.Dao.Order
+{
+  public  class ClienterPushLogDao : DaoBase
+    {
+        /// <summary>
+        /// 增加一条记录
+        /// </summary>
+        public long Insert(ClienterPushLog clienterPushLog)
+        {
+            const string insertSql = @"
+insert into ClienterPushLog(OrderId,ClienterIds)
+values(@OrderId,@ClienterIds)
+
+select @@IDENTITY";
+            IDbParameters dbParameters = DbHelper.CreateDbParameters();
+            dbParameters.AddWithValue("OrderId", clienterPushLog.OrderId);
+            dbParameters.AddWithValue("ClienterIds", clienterPushLog.ClienterIds);
+
+            return ParseHelper.ToLong(DbHelper.ExecuteScalar(base.SuperMan_Write, insertSql, dbParameters));
+        }
+
+        /// <summary>
+        /// 更新一条记录
+        /// </summary>
+        public void Update(ClienterPushLog clienterPushLog)
+        {
+            const string updateSql = @"
+update  ClienterPushLog
+set  OrderId=@OrderId,ClienterIds=@ClienterIds
+where  ID=@ID ";
+            IDbParameters dbParameters = DbHelper.CreateDbParameters();
+            dbParameters.AddWithValue("ID", clienterPushLog.ID);
+            dbParameters.AddWithValue("OrderId", clienterPushLog.OrderId);
+            dbParameters.AddWithValue("ClienterIds", clienterPushLog.ClienterIds);
+            DbHelper.ExecuteNonQuery(SuperMan_Write, updateSql, dbParameters);
+        }
+
+
+    }
+}
---
+++ Ets.Model/DataModel/Order/ClienterPushLog.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ets.Model.DataModel.Order
+{
+    /// <summary>
+    /// 实体类ClienterPushLog 。(属性说明自动提取数据库字段的描述信息)
+    /// Generate By: wangyuchuan
+    /// Generate Time: 2015-12-31 14:42:37
+    /// </summary>
+    public class ClienterPushLog
+    {
+        public ClienterPushLog() { }
+        /// <summary>
+        /// 主键 自增ID
+        /// </summary>
+        public long ID { get; set; }
+        /// <summary>
+        /// 订单id
+        /// </summary>
+        public long OrderId { get; set; }
+        /// <summary>
+        /// 推送骑士id集合 以;id;id;格式存储
+        /// </summary>
+        public string ClienterIds { get; set; }
+        /// <summary>
+        /// 推送时间
+        /// </summary>
+        public DateTime CreateTime { get; set; }
+
+    }
+
+}
