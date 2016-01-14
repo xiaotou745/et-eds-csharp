4fc52b08de67f05e3f8a7240ce1d282650349346
douhaichao <dou631@163.com>
2016-01-14 09:46:53 +0800

    微信回调

--- ETS.Library/Pay/SSBWxPay/Notify.cs
+++ ETS.Library/Pay/SSBWxPay/Notify.cs
@@ -27,26 +27,17 @@ namespace ETS.Library.Pay.SSBWxPay
         public WxPayData GetNotifyData()
         {
             //接收从微信后台POST过来的数据
-
-            string builder = string.Empty;
-            using (System.IO.Stream stream = HttpContext.Current.Request.InputStream)
+            System.IO.Stream s = HttpContext.Current.Request.InputStream;
+            int count = 0;
+            byte[] buffer = new byte[1024];
+            StringBuilder builder = new StringBuilder();
+            while ((count = s.Read(buffer, 0, 1024)) > 0)
             {
-                Byte[] postBytes = new Byte[stream.Length];
-                stream.Read(postBytes, 0, (Int32)stream.Length);
-                builder = Encoding.UTF8.GetString(postBytes);
+                builder.Append(Encoding.UTF8.GetString(buffer, 0, count));
             }
-
-            //System.IO.Stream s =HttpContext.Current.Request.InputStream;
-            //int count = 0;
-            //byte[] buffer = new byte[1024];
-            //StringBuilder builder = new StringBuilder();
-            //while ((count = s.Read(buffer, 0, 1024)) > 0)
-            //{
-            //    builder.Append(Encoding.UTF8.GetString(buffer, 0, count));
-            //}
-            //s.Flush();
-            //s.Close();
-            //s.Dispose();
+            s.Flush();
+            s.Close();
+            s.Dispose();
 
             Log.Info(this.GetType().ToString(), "Receive data from WeChat : " + builder.ToString());
 
@@ -56,17 +47,15 @@ namespace ETS.Library.Pay.SSBWxPay
             {
                 data.FromXml(builder.ToString());
             }
-            catch(WxPayException ex)
+            catch (WxPayException ex)
             {
                 //若签名错误，则立即返回结果给微信支付后台
-                //WxPayData res = new WxPayData();
-                //res.SetValue("return_code", "FAIL");
-                //res.SetValue("return_msg", ex.Message);
-                //Log.Error(this.GetType().ToString(), "Sign check error : " + res.ToXml());
+                WxPayData res = new WxPayData();
+                res.SetValue("return_code", "FAIL");
+                res.SetValue("return_msg", ex.Message);
+                Log.Error(this.GetType().ToString(), "Sign check error : " + res.ToXml());
                 //page.Response.Write(res.ToXml());
                 //page.Response.End();
-                data.SetValue("return_code", "FAIL");
-                data.SetValue("return_msg", ex.Message);
             }
 
             Log.Info(this.GetType().ToString(), "Check sign success");
