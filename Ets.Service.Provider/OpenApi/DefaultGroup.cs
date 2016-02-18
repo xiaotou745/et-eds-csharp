40429db8c1cb099a8e9c395c4fa9498a8d33e72d
caoheyang <cao.heyang@etaostars.com>
2016-02-18 10:52:17 +0800

    记录 回掉请求

--- Ets.Service.Provider/OpenApi/DefaultGroup.cs
+++ Ets.Service.Provider/OpenApi/DefaultGroup.cs
@@ -5,8 +5,10 @@ using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using ETS.Const;
+using Ets.Dao.Order;
 using ETS.Enums;
 using Ets.Model.Common;
+using Ets.Model.DataModel.Order;
 using Ets.Model.ParameterModel.Order;
 using Ets.Service.IProvider.OpenApi;
 using ETS.Util;
@@ -31,19 +33,29 @@ namespace Ets.Service.Provider.OpenApi
             {
                 app_key = paramodel.app_key,
                 timestamp = paramodel.timestamp,
-                v=paramodel.v,
-                sign=paramodel.sign,
+                v = paramodel.v,
+                sign = paramodel.sign,
                 fields = new
                 {
-                    order_no=paramodel.fields.OriginalOrderNo,  //第三方平台订单号
-                    status=paramodel.fields.status, //E代送订单状态
-                    clientername=paramodel.fields.ClienterTrueName, //配送员姓名
+                    order_no = paramodel.fields.OriginalOrderNo,  //第三方平台订单号
+                    status = paramodel.fields.status, //E代送订单状态
+                    clientername = paramodel.fields.ClienterTrueName, //配送员姓名
                     clienterphone = paramodel.fields.ClienterPhoneNo, //配送员电话
                     businessName = paramodel.fields.BusinessName, //商户名
-                    cancelreason=paramodel.fields.OtherCancelReason //订单取消原因
+                    cancelreason = paramodel.fields.OtherCancelReason //订单取消原因
                 }
             };
             string json = new HttpClient().PostAsJsonAsync(paramodel.fields.ReturnUrl, paramodel).Result.Content.ReadAsStringAsync().Result;
+            int r = new OpenCallBackLogDao().Insert(new OpenCallBackLog()
+             {
+                 Url = paramodel.fields.ReturnUrl,
+                 OrderId = 0,
+                 OrderNo = paramodel.fields.order_no,
+                 RequestBody = JsonHelper.ToJson(p),
+                 ResponseBody = json,
+                 Status = paramodel.fields.status
+             });
+
             if (!string.IsNullOrWhiteSpace(json) && json == "success")
             {
                 return OrderApiStatusType.Success;
